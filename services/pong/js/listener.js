ad33f8c59189f6f3ce3f846ca74c6ae370bae115
Charlotte Sweeting <charlotte@mail>
2025-07-30 19:51:58 +0200

    chore: testing

--- services/pong/js/listener.js
+++ services/pong/js/listener.js
@@ -1,11 +1,16 @@
 
 import http from 'http';
-const port = 2020;
+import fs from 'fs';
+const port = process.env.PORT;
+//va recupérer la valeur de PORT dans le fichier .env
+//pour qu'à l'exec ça fonctionne il faut faire "node --env-file=.env listener.js"
 
 const server = http.createServer((req, res) => {
+	console.log(req.url);
+	console.log(req.method);
 	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Pong Microservice Listener');
+	res.setHeader('Content-Type', 'text/html');
+	res.end('<h1>Pong Microservice Listener</h1>');
 })
 
 server.listen(port, () => {
