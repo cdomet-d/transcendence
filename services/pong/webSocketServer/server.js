4a8978f5dc72d2de65dc539ca76cbba8f6e08280
Charlotte Sweeting <charlottesweeting@outlook.fr>
2025-08-05 16:13:58 +0200

    fix: added more options to fastify server and cleaned code

--- services/pong/webSocketServer/server.js
+++ services/pong/webSocketServer/server.js
@@ -1,38 +1,71 @@
 import Fastify from 'fastify'
-import fs from 'fs';
-// import WebSocket from 'ws';
 // importe la fonction constructeur Fastify depuis le module fastify.
 // ici c'est seulement l'objet en lui meme qui est appelé, cad sa référence
 // mais la fonction n'est pas executé
+import fs from 'fs/promises';
+const port = process.env.PORT;
+//"node --env-file=.env server.js"
 
-const fastify = Fastify({
-  logger: true
-})
+/* creer le server */
+function checkProxy(address, hop) {
+  if (address === '127.0.0.1'/*adresse nginx*/ || hop === 1)
+    return true;
+  return false;
+}
+
+const options = {
+  logger: true,
+  https: true,
+  trustProxy: checkProxy
+  //connectionTimeout
+  //forceCloseConnections
+  //pluginTimeout
+}
+
+const fastifyServer = Fastify(options)
 // cette fois la fonction constructeur Fastify est executée grace aux parenthèses
 // elle retourne une instance du server fastify (un objet)
-// et le configure pour activer le logger pino
+// elle prend un argument: un objet 'options'
 
-fastify.get('/api/game/match', function (request, reply) {
-  const stream = fs.createReadStream('app/wsReply.js');
-  reply.header('Content-Type', 'application/javascript');
-  reply.send(stream);
-})
+/*creer les routes avec leur handlers, hooks, decorators, plugins*/
+const opts = {
+  schema: {
+    //TODO: get coralie's json object
+  }
+}
 
-fastify.listen({ port: 2020 }, function (err, address) {
+function handler(req, rep) {
+  // const stream = fs.createReadStream('app/wsReply.js');
+  // rep.header('Content-Type', 'application/javascript');
+  // rep.send(stream);
+}
+
+fastifyServer.get('/game/match', opts, handler)
+//lorsque le server recoit une requete GET avec l'url /game/match la fonction handler sera execute
+
+/*lancer le server*/
+fastifyServer.listen({ port: port }, catchError)
+
+function catchError(err, address) {
   if (err) {
-    fastify.log.error(err);
+    fastifyServer.log.error(err);
     process.exit(1);
   }
-})
+  fastifyServer.log.info(`Pong Microservice listening on ${port} at ${address}`);
+}
+//fonction hoistee avec sa propre portee de this a l'oppose d'une fonction flechee
 
-// const wsserver = new WebSocket.Server({ port: 2222 });
-
-// wsserver.on('connection', function connection(ws) {
-//   console.log('Client connecté');
-
-//   ws.on('message', function incoming(message) {
-//     console.log('Reçu du client:', message);
-//     ws.send(`Réponse du serveur : ${message}`);
-//   });
-// });
-
+/*
+server steps:
+- creer un socket non-blocant, associe a localhost, 
+  qui ecoute des demandes de connexion sur des routes specifiques
+  et qui suis le protocol https et wss
+- creer les routes
+- lancer le server (tourne tant qu'il est pas arrete par un ctrl C)
+- a chaque evenement sur la socket server: 
+  soit accepter le client, soit analyser la requete wss
+- pour accepter client: verifier la route, 
+  renvoyer un javascript qui sera executer dans le navigateur du client 
+  pour faire une demande de connexion en websocket, 
+  accepter la requete et maintenir une connexion bidirectionnelle avec le client
+*/
