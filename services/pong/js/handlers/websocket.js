b77ea9982ce2c4d1087ded8a5e79fb4c5a5a94b5
Charlotte Sweeting <charlotte@mail>
2025-08-08 14:08:43 +0200

    chore: small change

--- services/pong/js/handlers/websocket.js
+++ services/pong/js/handlers/websocket.js
@@ -8,6 +8,7 @@ async function upgrade(req, rep) {
 
 function wshandler(socket, req) {
   req.server.log.info('WebSocket connection established');
+
   socket.on('message', (message) => {
       socket.send('hi from server');
       req.server.log.info(`client sent: ${message}`);
