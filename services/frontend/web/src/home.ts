7bb670831592eb81d8ea4975708d6436ecf6ec8a
Alexie Julien <aljulien@student.42lyon.fr>
2025-08-20 12:58:59 +0200

    chore : changed 'play' to 'Play'

--- services/frontend/web/src/home.ts
+++ services/frontend/web/src/home.ts
@@ -20,10 +20,10 @@ export function renderHome(main: HTMLElement) {
           data-link
           id="play-btn"
           class="relative flex items-center justify-center px-20 py-8 rounded-full bg-gradient-to-br from-[#ea9800] to-[#ffcc00] shadow-xl hover:shadow-2xl hover:scale-105 transition-all duration-200 group text-white font-bold text-5xl uppercase tracking-widest"
-          title="${t('play')}"
+          title="${t('Play')}"
         >
           <span class="absolute inset-0 rounded-full border-2 border-black/80 pointer-events-none"></span>
-          <span class="relative z-10">${t('play')}</span>
+          <span class="relative z-10">${t('Play')}</span>
         </a>
       </div>
     </div>
