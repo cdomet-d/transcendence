f7abe1ed51832f3a363d7ca72f1cdc7bcdd40fd1
Charlotte Sweeting <charlottesweeting@outlook.fr>
2025-08-28 18:34:46 +0200

    add: methods for drawing paddles, ball and middle line

--- services/pong/api/src/frontend/pong.ts
+++ services/pong/api/src/frontend/pong.ts
@@ -5,23 +5,16 @@ const HEIGHT = 270;
 
 wsRequest();
 
-// const body = document.body as HTMLElement;
-// body.className = "min-h-screen font-sans text-[#f1a541] bg-lavender flex flex-col justify-center items-center content-center gap-[50px]";
-
 const game = document.getElementById("game") as HTMLElement;
 game.className = "min-h-screen flex flex-col items-center justify-center bg-white"
-// "border-[5px] border-[#f7d793] bg-[antiquewhite] flex flex-col items-center";
+// "border-[5px] border-[#f7d793] bg-[antiquewhite]";
 
 const h1 = document.querySelector('h1') as HTMLElement;
 h1.className = "text-4xl font-bold mb-6";
 // "m-2.5 p-2.5 bg-beige";
 
-// const backLink = document.querySelector('a') as HTMLElement;
-// backLink.className = "mt-4 py-3 px-8 rounded-full border-1 border-black bg-gradient-to-br from-[#ffcc00] to-[#ea9800] shadow-md hover:scale-105 transition-all text-white text-lg font-semibold"
-
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-canvas.className = "border mb-6";
-// "bg-aliceblue border-4 border-[#8ec7fc] rounded-[20px] m-12";
+canvas.className = "border mb-6 bg-aliceblue border-4 border-[#8ec7fc] rounded-[20px]";
 
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
@@ -33,16 +26,45 @@ if (ctx === null) {
     process.exit(1);
 }
 
-ctx.fillRect(10, 108, 10, 54);
-ctx.fillRect(460, 108, 10, 54);
+function drawPaddle(ctx: CanvasRenderingContext2D, x: number, y: number) {
+    ctx.fillRect(10 + x, 108 + y, 10, 54);
+    ctx.fillRect(460 + x, 108 + y, 10, 54);
+}
 
-ctx.beginPath();
-ctx.moveTo(WIDTH / 2, 0);
-ctx.lineTo(WIDTH / 2, HEIGHT);
-ctx.stroke();
+function drawMiddleLine(ctx: CanvasRenderingContext2D) {
+    ctx.beginPath();
+    ctx.moveTo(WIDTH / 2, 0);
+    ctx.lineTo(WIDTH / 2, HEIGHT);
+    ctx.stroke();
+}
 
-ctx.beginPath();
-ctx.arc(WIDTH / 2, HEIGHT / 2, 10, 0, Math.PI * 2, false);
-ctx.fill();
+function drawBall(ctx: CanvasRenderingContext2D) {
+    ctx.beginPath();
+    ctx.arc(WIDTH / 2, HEIGHT / 2, 10, 0, Math.PI * 2, false);
+    ctx.fill();
+}
+
+function keyEventHandler(event: KeyboardEvent): void {
+    if (event.key === "ArrowUp")
+        ;        
+    if (event.key === "ArrowDown")
+        ;
+}
+
+interface paddelPos {
+    x: number;
+    y: number;
+}
+
+function renderGame(ctx: CanvasRenderingContext2D) {
+    let leftPad :paddelPos = {10, 108};
+    drawMiddleLine(ctx);
+    drawMiddleLine(ctx);
+    drawBall(ctx);
+}
+
+renderGame(ctx);
+
+window.addEventListener("keydown", keyEventHandler);
 
 // window.addEventListener("load", );
