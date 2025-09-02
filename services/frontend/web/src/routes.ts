af4f1f2c8ca58f0b7d7a87eadc9c68ad4108235e
Charlotte Sweeting <charlotte@mail>
2025-09-02 20:59:37 +0200

    fix: put same name routes as in nginx config

--- services/frontend/web/src/routes.ts
+++ services/frontend/web/src/routes.ts
@@ -1,17 +1,17 @@
 import { renderHome } from './home.js';
-import { renderGame } from './game.js';
 import { render404 } from './404.js';
 import { renderCentral } from './central.js';
 import { renderProfile } from './profile.js';
 import { renderLeaderboard } from './leaderboard.js';
 import { renderTournament } from './tournament.js';
+import { wsRequest } from './pong/wsreply.js';
 
 export const routes = [
   { path: '/', callback: renderHome },
-  { path: '/game', callback: renderGame },
   { path: '/404', callback: render404 },
-  { path: '/tournament', callback: renderTournament },
   { path: '/central', callback: renderCentral },
-  { path: '/profile', callback: renderProfile },
-  { path: '/leaderboard', callback: renderLeaderboard },
+  { path: '/account', callback: renderProfile },
+  { path: '/game/leaderboard', callback: renderLeaderboard },
+  { path: '/game/tournament', callback: renderTournament },
+  {path: '/game/match', callback: wsRequest},
 ];
