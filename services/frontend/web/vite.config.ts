6c626925ba4117988df027985098718be97f6fce
Charlotte Sweeting <charlotte@mail>
2025-09-04 10:59:49 +0200

    rm: build config

--- services/frontend/web/vite.config.ts
+++ services/frontend/web/vite.config.ts
@@ -1,17 +1,8 @@
 import viteFastify from '@fastify/vite/plugin'
 
 export default {
-  root: "/app/static",
+  root: "./src/client",
   plugins: [
     viteFastify(),
   ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: "/app/static/index.html",
-        nested: "/app/src/client/main.ts"
-      }
-    },
-    outDir: '/app/dist/client',
-  },
 }
