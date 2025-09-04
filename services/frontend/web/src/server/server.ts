7f43b1f1895173372b4c191b91797baa861ae23b
Charlotte Sweeting <charlotte@mail>
2025-09-04 11:27:30 +0200

    rm: static fastify cause vite handles it

--- services/frontend/web/src/server/server.ts
+++ services/frontend/web/src/server/server.ts
@@ -1,6 +1,5 @@
 import Fastify from 'fastify'
 import type { FastifyInstance } from 'fastify';
-import fastifyStatic from '@fastify/static'
 import { servRoutes } from './route.js';
 import { options } from './serv.conf.js';
 import fastifyVite from '@fastify/vite'
@@ -20,18 +19,9 @@ catch (err) {
 }
 
 async function addPlugins(serv: FastifyInstance) {
-	await serv.register(fastifyStatic, {
-			root: [
-				"/app/static/",
-				"/app/images/",
-				"/app/dist/client/",
-				"/app/dist/client/pong/",
-			],
-		})
-		.register(servRoutes)
+	await serv.register(servRoutes)
 		.register(fastifyVite, {
 			root: "/app",
-			clientModule: "/app/src/client/main.ts",
 			dev: process.argv.includes('--dev'),
 			spa: true,
 		});
