f354cdb311bb18ad84486e275669126163d86389
Charlotte Sweeting <charlotte@mail>
2025-09-04 11:27:52 +0200

    add: tailwindcss plugin to vite

--- services/frontend/web/vite.config.ts
+++ services/frontend/web/vite.config.ts
@@ -1,8 +1,10 @@
 import viteFastify from '@fastify/vite/plugin'
+import tailwindcss from '@tailwindcss/vite'
 
 export default {
   root: "./src/client",
   plugins: [
     viteFastify(),
+    tailwindcss(),
   ],
 }
