eaa3366beed06a359c3094dccfd258cdeda2e0d2
Charlotte Sweeting <charlotte@mail>
2025-09-04 12:29:43 +0200

    add: rep.html for vite reload

--- services/frontend/web/src/server/route.ts
+++ services/frontend/web/src/server/route.ts
@@ -3,15 +3,16 @@ import type { FastifyPluginCallback } from 'fastify';
 import type { FastifyRequest, FastifyReply } from 'fastify';
 
 async function handler(req: FastifyRequest, rep: FastifyReply) {
-    try {
-        const script = await fsp.readFile("/app/src/client/index.html");
-        rep.header('Content-Type', 'text/html');
-        rep.send(script);
-    }
-    catch (err) {
-        const error = err as NodeJS.ErrnoException;
-        rep.code(500).send(error.message);
-    }
+    // try {
+    //     const script = await fsp.readFile("/app/src/client/index.html");
+    //     rep.header('Content-Type', 'text/html');
+    //     rep.send(script);
+    // }
+    // catch (err) {
+    //     const error = err as NodeJS.ErrnoException;
+    //     rep.code(500).send(error.message);
+    // }
+    rep.html(); //for vite
 }
 
 const servRoutes: FastifyPluginCallback = function (serv, options, done) {
