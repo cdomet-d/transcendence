ebe8b8f02d65b4d1aeee4ba4a72e57f4d45ae524
Charlotte Sweeting <charlottesweeting@email>
2025-09-08 14:05:18 +0200

    add: test for websocket connection

---
+++ services/pong/app/test/server.test.ts
@@ -0,0 +1,42 @@
+import { init } from '../src/server.ts';
+import type { FastifyInstance } from 'fastify';
+import {describe, expect, test, afterAll, beforeAll} from '@jest/globals';
+import WebSocket from 'ws';
+
+describe('Fastify server', () => {
+    let server: FastifyInstance;
+    let address: string;
+
+    beforeAll(async () => {
+        server = init();
+        await server.ready();
+        address = await server.listen({ port: 0 });
+    });
+
+    afterAll(async () => {
+        await server.close();
+    });
+
+    test('WebSocket should respond correctly', (done) => {
+        const wsUrl = address.replace(/^http/, 'ws') + "/api/game/match";
+
+        const ws = new WebSocket(wsUrl);
+
+        ws.on('open', () => {
+            ws.send('Hey server');
+        });
+
+        ws.on('message', (data) => {
+            expect(data.toString()).toBe('Hey from server');
+            ws.close();
+        });
+
+        ws.on('close', () => {
+            done();
+        });
+
+        ws.on('error', (err) => {
+            done(err);
+        });
+    });
+});
