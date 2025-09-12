fb329e6a5502d5a62105c6fc41bac19629f31d36
Charlotte Sweeting <charlottesweeting@email>
2025-09-12 12:26:03 +0200

    fix: added back translate in renderHome

--- services/frontend/app/src/pages/html.pages.ts
+++ services/frontend/app/src/pages/html.pages.ts
@@ -2,6 +2,8 @@ import { translate } from '../client/scripts/language/translation.js';
 import { renderPageTemplate } from './page.template.js'
 
 export function renderHome(): string {
+	console.log("IN HOME");
+	console.log(translate('Play'));
 	let html = `
 		<div class="min-h-screen flex items-center justify-center bg-white">
 			<a
@@ -9,10 +11,10 @@ export function renderHome(): string {
 				data-link
 				id="play-btn"
 				class="relative flex items-center justify-center px-20 py-8 rounded-full bg-gradient-to-br from-[#ea9800] to-[#ffcc00] shadow-xl hover:shadow-2xl hover:scale-105 transition-all duration-200 group text-white font-bold text-5xl uppercase tracking-widest"
-				title="Play"
+				title="${translate('Play')}"
 			>
 				<span class="absolute inset-0 rounded-full border-2 border-black/80 pointer-events-none"></span>
-				<span class="relative z-10">Play</span>
+				<span class="relative z-10">${translate('Play')}</span>
 			</a>
 		</div>
 	`;
