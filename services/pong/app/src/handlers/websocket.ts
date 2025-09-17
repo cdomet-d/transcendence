21aaaf5945b8ee34cf288c9287489487fbb334f1
Charlotte Sweeting <charlottesweeting@email>
2025-09-17 18:14:31 +0200

    add: in comments object that will be received from gameManager

--- services/pong/app/src/handlers/websocket.ts
+++ services/pong/app/src/handlers/websocket.ts
@@ -2,6 +2,22 @@ import type { FastifyRequest } from 'fastify';
 import type { WebSocket } from '@fastify/websocket';
 import { paddlePos } from './pong.js';
 
+/*
+match:
+	-   matchID
+	-   score
+	-   local: bool
+	-   users[]:
+		-   user1:
+			-   username
+			-   userID
+		-   user2:
+			-   username
+			-   userID
+	-   winner
+	-   loser
+*/
+
 function wshandler(socket: WebSocket, req: FastifyRequest): void {
 	req.server.log.info('WebSocket connection established');
 
