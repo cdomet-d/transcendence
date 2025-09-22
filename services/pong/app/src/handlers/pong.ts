1900a4a1d415467b9ed277abbb58d76e5bc9ab10
Charlotte Sweeting <charlottesweeting@email>
2025-09-22 17:10:00 +0200

    add: events for paddle movements

--- services/pong/app/src/handlers/pong.ts
+++ services/pong/app/src/handlers/pong.ts
@@ -1,8 +1,33 @@
 import type { WebSocket } from '@fastify/websocket';
+import type { Game } from '../classes/game.class.js';
+import { paddlePos } from './paddle.js';
+import type { Player } from '../classes/player.class.js';
 
-export function setUpGame() {
+export function setUpGame(game: Game) {
+    game.players[0]?.setLeftPaddle();
+    game.players[1]?.setRightPaddle();
 
+    //TODO: send info to display game screen (users names)
+    startGame(game);
 }
 
-// if (mess.match("Pad:"))
-// 	paddlePos(socket, mess);
+function startGame(game: Game) {
+    const leftPlayer = game.players[0];
+    const rightPlayer = game.players[1];
+    if (!leftPlayer || !rightPlayer)
+        return;
+
+    let keys: string[] = ["w", "s"];
+    setPaddleEvent(leftPlayer, keys);
+    if (game.local)
+        keys = ["ArrowUp", "ArrowDown"];
+    setPaddleEvent(rightPlayer, keys);
+}
+
+function setPaddleEvent(player: Player, keys: string[]) {
+    player.socket.on("message", (payload: any) => {
+        const mess: string = payload.toString();
+		if (mess === keys[0] || mess === keys[1])
+			paddlePos(player, mess);
+    })
+}
