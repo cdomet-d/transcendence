82d76d41a4b47c1a976d5fa7835d26fb1e01f398
Charlotte Sweeting <charlottesweeting@email>
2025-09-23 19:23:01 +0200

    fix: rm event on open cause not necessary

--- services/pong/app/src/handlers/websocket.ts
+++ services/pong/app/src/handlers/websocket.ts
@@ -6,39 +6,37 @@ import { setUpGame } from './pong.js';
 
 function wshandler(this: FastifyInstance, socket: WebSocket, req: FastifyRequest): void {
 	this.log.info('WebSocket connection established');
+	const GameIdCookie: string | undefined = req.cookies.gameid;
+	const UserIdCookie: string | undefined = req.cookies.userid;
 
-	socket.on('open', () => {
-		const GameIdCookie: string | undefined = req.cookies.gameid;
-		const UserIdCookie: string | undefined = req.cookies.userid;
+	const userID: number = Number(UserIdCookie);
+	const gameID: number = Number(GameIdCookie);
+	if (Number.isNaN(gameID) || Number.isNaN(userID))
+		throw new Error("wrong ID"); //TODO: catch error
 
-		const userID: number = Number(UserIdCookie);
-		const gameID: number = Number(GameIdCookie);
-		if (Number.isNaN(gameID) || Number.isNaN(userID))
-			throw new Error("wrong ID"); //TODO: catch error
+	const game: Game | undefined = this.gameRegistry.findGame(gameID);
+	if (!game) 
+		throw new Error("game not found"); //TODO: send nats message ?
 
-		const game: Game | undefined = this.gameRegistry.findGame(gameID);
-		if (!game) 
-			throw new Error("game not found"); //TODO: send nats message ?
+	if (game.players.length === 2) {
+		// socket.close();
+		throw new Error("not allowed"); //errorHandler should be called
+	}
 
-		if (game.players.length === 2) {
-			// socket.close();
-			throw new Error("not allowed"); //errorHandler should be called
-		}
-
-		game.addPlayer(new Player(userID, socket));
-		if (game.local) {
-			const randUserIdCookie: string | undefined = req.cookies.randuserid;
-			const randUserId: number = Number(randUserIdCookie);
-			if (Number.isNaN(randUserId))
-				throw new Error("wrong ID");
-			game.addPlayer(new Player(randUserId, socket));
-		}
-		
-		if (game.players.length === 2)
-			setUpGame(game);
-	});
+	game.addPlayer(new Player(userID, socket));
+	if (game.local) {
+		const randUserIdCookie: string | undefined = req.cookies.randuserid;
+		const randUserId: number = Number(randUserIdCookie);
+		if (Number.isNaN(randUserId))
+			throw new Error("wrong ID");
+		game.addPlayer(new Player(randUserId, socket));
+	}
+	
+	if (game.players.length === 2)
+		setUpGame(game);
 
 	socket.on('close', () => {
+		game.deletePlayers(); //TODO: to be rm
 		//TODO: delete game ? need to know if everything is done
 	});
 }
