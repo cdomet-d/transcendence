5007f519d9326a72a666e69b5f965bb736239865
Charlotte Sweeting <charlottesweeting@email>
2025-09-24 16:28:40 +0200

    fix: replaced keys string with keys obj and rm socket.send

--- services/pong/app/src/handlers/paddle.ts
+++ services/pong/app/src/handlers/paddle.ts
@@ -1,32 +1,23 @@
-import type { WebSocket } from '@fastify/websocket';
-import type { paddlePos } from '../classes/player.class.js';
-import type { Player } from '../classes/player.class.js';
+import type { paddlePos, Player } from '../classes/player.class.js';
+import type { keysObj } from './pong.js';
 
 const WIDTH = 480;
 const HEIGHT = 270;
 const padSpeed: number = 7;
 
-export function paddlePos(player: Player, key: string) {
-    if (key === "w" || key === "ArrowUp") {
-        up(player.paddle, player.socket);
-    }
-    if (key === "s" || key === "ArrowDown") {
-        down(player.paddle, player.socket);
-    }
+export function updatePaddlePos(player: Player, keys: keysObj) {
+    if (keys.w || keys.ArrowUp)
+        up(player.paddle);
+    if (keys.s || keys.ArrowDown)
+        down(player.paddle);
 }
 
-function up(pad: paddlePos, socket: WebSocket) {
+function up(pad: paddlePos) {
     if (pad.y - padSpeed > 0)
         pad.y -= padSpeed;
-    if (socket.readyState === 1)
-        socket.send(pad.y.toString());
-    //else
 }
 
-function down(pad: paddlePos, socket: WebSocket) {
+function down(pad: paddlePos) {
     if (pad.y + padSpeed < (HEIGHT - 54))
         pad.y += padSpeed;
-    if (socket.readyState === 1)
-        socket.send(pad.y.toString());
-    //else
 }
