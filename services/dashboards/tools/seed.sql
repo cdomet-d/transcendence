9da5f05874375a5cecb91642a716fdc56f70e476
Eiixela <alexielaurine@live.fr>
2025-09-25 13:17:00 +0200

    add: script bash and sql for dashboard DB

---
+++ services/dashboards/tools/seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO gameMatchInfo (gameID, duration, startTime, status, hostID, winnerID, invitedID) VALUES (1, 90, '2025-09-04 12:00:00', 2, 1, 1, 2);
+INSERT INTO gameLobby (lobbyID, status) VALUES (1, 1);
