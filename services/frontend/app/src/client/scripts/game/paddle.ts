4c9bb231de5424d6889a44c2d3c32b4e4c708b72
Charlotte Sweeting <charlottesweeting@email>
2025-09-25 14:38:53 +0200

    fix: added preventDefault to key events to stop window from moving when pressing arrows

--- services/frontend/app/src/client/scripts/game/paddle.ts
+++ services/frontend/app/src/client/scripts/game/paddle.ts
@@ -22,6 +22,7 @@ export function addMessEvent(leftPad: game.paddlePos, rightPad: game.paddlePos, 
 // import { keyChange } from './pong.js';
 export function createKeyDownEvent(keys: game.keysObj)  {
 	return function keyDownEvent(event: KeyboardEvent): void {
+		event.preventDefault();
 		if (event.key === "w")
 			keys._w = true;
 		if (event.key === "s")
@@ -35,6 +36,7 @@ export function createKeyDownEvent(keys: game.keysObj)  {
 
 export function createKeyUpEvent(keys: game.keysObj)  {
 	return function keyUpEvent(event: KeyboardEvent): void {
+		event.preventDefault();
 		if (event.key === "w")
 			keys._w = false;
 		if (event.key === "s")
