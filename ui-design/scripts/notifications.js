ab7198a90c520b2fbf547e61bf2c31d72dc3c925
Jauppp <coralie.dometdemont@gmail.com>
2025-09-26 11:45:49 +0200

    add: dancing letter animation and notification + language menu in header

--- ui-design/scripts/notifications.js
+++ ui-design/scripts/notifications.js
@@ -1,23 +1,30 @@
 // displays a div on click on one element.
-// The toggle must have id="clickable"
-// The container to show must have id="notificationInteractiveWrapper"
-// The container to hide must have id="unreadNotificationAlert"
+// The toggle must have id="notifToggle"
+// The container to show must have id="notifPopup"
+// The container to hide must have id="notifAlert"
 // getElementById returns the first occurence of the id it finds;
 // be mindful of id uniqueness!
-var clickedDiv = document.getElementById("clickable");
+var clickedDiv = document.getElementById("notifToggle");
 if (clickedDiv) {
     clickedDiv.addEventListener("click", function () {
-        var notificationInteractiveWrapper = document.getElementById("notificationInteractiveWrapper");
-        var unreadNotificationAlert = document.getElementById("unreadNotificationAlert");
-        if (notificationInteractiveWrapper) {
-            if (notificationInteractiveWrapper.hasAttribute("selected"))
-                notificationInteractiveWrapper.removeAttribute("selected");
+        var notifPopup = document.getElementById("notifPopup");
+        var notifAlert = document.getElementById("notifAlert");
+        if (notifPopup) {
+            if (notifPopup.hasAttribute("selected"))
+                notifPopup.removeAttribute("selected");
             else
-                notificationInteractiveWrapper.setAttribute("selected", "");
+                notifPopup.setAttribute("selected", "");
+            var pos = clickedDiv.getBoundingClientRect();
+            var popupWidth = notifPopup.offsetWidth;
+            var pOffsetLeft = pos.left - popupWidth;
+            var pOffsetTop = pos.top - 8;
+            notifPopup.style.position = "fixed";
+            notifPopup.style.left = "".concat(pOffsetLeft, "px");
+            notifPopup.style.top = "".concat(pOffsetTop, "px");
         }
-        if (unreadNotificationAlert) {
-            if (unreadNotificationAlert.hasAttribute("selected"))
-                unreadNotificationAlert.removeAttribute("selected");
+        if (notifAlert) {
+            if (notifAlert.hasAttribute("selected"))
+                notifAlert.removeAttribute("selected");
         }
     });
 }
--- ui-design/scripts/notifications.ts
+++ ui-design/scripts/notifications.ts
@@ -1,29 +1,34 @@
 // displays a div on click on one element.
-// The toggle must have id="clickable"
-// The container to show must have id="notificationInteractiveWrapper"
-// The container to hide must have id="unreadNotificationAlert"
+// The toggle must have id="notifToggle"
+// The container to show must have id="notifPopup"
+// The container to hide must have id="notifAlert"
 // getElementById returns the first occurence of the id it finds;
 // be mindful of id uniqueness!
 
-const clickedDiv = document.getElementById("clickable");
+const clickedDiv = document.getElementById("notifToggle");
 
 if (clickedDiv) {
   clickedDiv.addEventListener("click", () => {
-    const notificationInteractiveWrapper = document.getElementById(
-      "notificationInteractiveWrapper"
-    );
-    const unreadNotificationAlert = document.getElementById(
-      "unreadNotificationAlert"
-    );
+    const notifPopup = document.getElementById("notifPopup");
+    const notifAlert = document.getElementById("notifAlert");
 
-    if (notificationInteractiveWrapper) {
-      if (notificationInteractiveWrapper.hasAttribute("selected"))
-        notificationInteractiveWrapper.removeAttribute("selected");
-      else notificationInteractiveWrapper.setAttribute("selected", "");
+    if (notifPopup) {
+      if (notifPopup.hasAttribute("selected"))
+        notifPopup.removeAttribute("selected");
+      else notifPopup.setAttribute("selected", "");
+    
+	  const pos = clickedDiv.getBoundingClientRect();
+      const popupWidth = notifPopup.offsetWidth;
+      const pOffsetLeft = pos.left - popupWidth;
+      const pOffsetTop = pos.top - 8;
+
+      notifPopup.style.position = "fixed";
+      notifPopup.style.left = `${pOffsetLeft}px`;
+      notifPopup.style.top = `${pOffsetTop}px`;
     }
-    if (unreadNotificationAlert) {
-      if (unreadNotificationAlert.hasAttribute("selected"))
-        unreadNotificationAlert.removeAttribute("selected");
+    if (notifAlert) {
+      if (notifAlert.hasAttribute("selected"))
+        notifAlert.removeAttribute("selected");
     }
   });
 }
