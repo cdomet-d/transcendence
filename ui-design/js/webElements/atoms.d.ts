38fd4e9cbec2a7ecf1cd674de4e153cfbb9fe991
Coralie Domet de Mont <cdomet-d@student.42lyon.fr>
2025-09-30 15:22:42 +0200

    chore: new directory structure

--- ui-design/js/webElements/atoms.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class menuButton extends HTMLButtonElement {
-    constructor();
-    connectedCallback(value: string): void;
-    render(value: string): void;
-}
-//# sourceMappingURL=atoms.d.ts.map
