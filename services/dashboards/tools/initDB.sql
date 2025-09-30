e080b3c9866a585c2a8dd84a1fa476d800727349
Eiixela <alexielaurine@live.fr>
2025-09-30 07:54:50 +0200

    chore: fixed dashboard database launch

--- services/dashboards/tools/initDB.sql
+++ services/dashboards/tools/initDB.sql
@@ -1,5 +1,5 @@
 CREATE TABLE gameMatchInfo (
-  gameID INTEGER AUTOINCREMENT,
+  gameID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   duration INTEGER,
   startTime DATETIME,
   gameStatus INTEGER, --game is still in lobby, game started, game ended
@@ -8,12 +8,11 @@ CREATE TABLE gameMatchInfo (
   tournamentID INTEGER,
   localGame BOOLEAN, --zero == local, 1 == remote
   scoreWinner INTEGER,
-  scoreLoser INTEGER,
-  PRIMARY KEY (gameID)
+  scoreLoser INTEGER
 );
 
-CREATE TABLE tournamentInfo {
-  tournamentID INTEGER PRIMARY AUTOINCREMENT NOT NULL, 
+CREATE TABLE tournamentInfo (
+  tournamentID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
   winnerID INTEGER,
   playerIDs TEXT -- Stores a JSON array like '[1, 5, 12, 23]'
-}
+);
