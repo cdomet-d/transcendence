4a185adac7b40694bb009b9dab6d0bec50a68f71
Eiixela <alexielaurine@live.fr>
2025-09-30 09:55:50 +0200

    add: SQL injection protection in building sql queries doc

--- services/accessibility/app/src/routes.ts
+++ services/accessibility/app/src/routes.ts
@@ -2,6 +2,7 @@ import type { FastifyInstance } from 'fastify';
 
 export async function languageRoutes(serv: FastifyInstance) {
 	
+	//get a translation from the word you want to translation + the targeted language
 	serv.get('/accessibility/translation', async (request, reply) => {
 		const { word: word} = request.params as { word: string };
 		const { langCode: langCode } = request.params as { langCode: string };
@@ -19,10 +20,10 @@ export async function languageRoutes(serv: FastifyInstance) {
 			const result = await serv.dbLanguage.get(query, [word, langCode]);
 
 			if (result) {
-		//Fastify assums a 200 code if data is returned
+				//Fastify assums a 200 code if data is returned
 				reply.send(result);
 			} else {
-		reply.code(404).send({ message: 'Translation not found' });
+				reply.code(404).send({ message: 'Translation not found' });
 			}
 		} catch (error) {
 			serv.log.error(error);
