13488de616067e37459674a9de396aa6e41ee388
Eiixela <alexielaurine@live.fr>
2025-09-30 11:12:16 +0200

    chore: changed routes in friends container to remove error

--- services/users/app/src/route.ts
+++ services/users/app/src/route.ts
@@ -1,26 +1,26 @@
 import type { FastifyInstance } from 'fastify';
 
+interface UserRow {
+  userID: number;
+  username: string;
+}
+
 export async function userRoutes(serv: FastifyInstance) {
-    serv.get('/internal/users/by-username/:username', async (request, reply) => {
-        try {
-            const { username } = request.params as{ username: string};
-            const query = `SELECT userID, username FROM userProfile WHERE username = ?`;
+	serv.get('/internal/users/by-username/:username', async (request, reply) => {
+		try {
+			const { username } = request.params as { username: string };
+			const query = `SELECT userID, username FROM userProfile WHERE username = ?`;
 
-            const user = await new Promise((resolve, reject) => {
-                serv.dbUsers.get(query, [username], (err, row) => {
-                    if (err) return reject(err);
-                    resolve(row);
-                });
-            });
-            if (!user) {
-                return (reply.code(404).send({message: 'User not found'}));
-            }
+			const user = await serv.dbUsers.get<UserRow>(query, [username]);
 
-            return (reply.code(200).send(user));
+			if (!user) {
+				return (reply.code(404).send({ message: 'User not found' }));
+			}
+			return (reply.code(200).send(user));
 
-        } catch (error) {
-            serv.log.error(error);
-            return (reply.code(500).send({message: 'Internal server error'}));
-        }
-    });
+		} catch (error) {
+			serv.log.error(error);
+			return reply.code(500).send({ message: 'Internal server error' });
+		}
+	});
 }
