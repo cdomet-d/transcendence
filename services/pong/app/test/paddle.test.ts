b20c6d946f8449c0f74c4718b9245699912e1f20
Charlotte Sweeting <charlottesweeting@email>
2025-10-01 13:32:57 +0200

    add: test when left player presses w

--- services/pong/app/test/paddle.test.ts
+++ services/pong/app/test/paddle.test.ts
@@ -1,7 +1,20 @@
 import {describe, expect, test, afterAll, beforeAll} from '@jest/globals';
 import { updatePaddlePos } from "../src/handlers/paddle";
 import type { Player } from '../src/classes/player.class';
+import type { keysObj } from '../src/handlers/pong';
 
 describe("paddle update", () => {
+	const player = {
+		paddle: { y: 108 },
+		left: true,
+		right: false,
+	} as unknown as Player;
 
+	test('left player moves paddle up when w key is pressed', () => {
+		const keys: keysObj = { _w: true, _s: false, _ArrowUp: false, _ArrowDown: false };
+		const delta = 5;
+		updatePaddlePos(player, keys, delta);
+		expect(player.paddle.y).toBe(107);
+	});
 });
+
