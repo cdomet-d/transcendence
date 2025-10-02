4097d1664cb25946dc8d391eccb73c7882f09002
Eiixela <alexielaurine@live.fr>
2025-10-02 19:11:31 +0200

    add: get activityStatus user function working

--- services/users/app/src/route.ts
+++ services/users/app/src/route.ts
@@ -38,14 +38,14 @@ export async function userRoutes(serv: FastifyInstance) {
 	//get user's profile with userID
 	serv.get('/internal/users/profile/:userId', async (request, reply) => {
 		try {
-			const { userId } = request.params as { userId: number };
+			const { userID } = request.params as { userID: number };
 
 			// Query your userProfile table for all the necessary info
 			const query = `
 				SELECT * FROM userProfile WHERE userID = ?
 			`;
 
-			const userProfile = await serv.dbUsers.get<UserProfile>(query, [userId]);
+			const userProfile = await serv.dbUsers.get<UserProfile>(query, [userID]);
 
 			if (!userProfile) {
 				return reply.code(404).send({ message: 'User profile not found' });
@@ -62,68 +62,172 @@ export async function userRoutes(serv: FastifyInstance) {
 
 	//get user's activity with userID
 	serv.get('/users/activity/:userID', async(request, reply) => {
+		try {
+			const { userID } = request.params as { userID: number };
+
+			const query = `
+				SELECT activityStatus FROM userProfile WHERE userID = ?
+			`;
+
+			const activityStatus = await serv.dbUsers.get<UserRow>(query, [userID]);
+			if (!activityStatus)
+				return (reply.code(404).send({ message: 'User not found' }));
+			return (reply.code(200).send(activityStatus));
+
+		} catch (error) {
+			serv.log.error(`Error fetching user profile: ${error}`);
+			return reply.code(500).send({ message: 'Internal server error' });
+		}
 	});
 
 	//get user's lastConnextion with userID
 	serv.get('/users/lastConnexion/:userID', async(request, reply) => {
+		try {
+			
+		} catch (error) {
+			serv.log.error(`Error fetching user profile: ${error}`);
+			return reply.code(500).send({ message: 'Internal server error' });
+		}
 	});
 
 	//update user's avatar with userID
 	serv.post('/users/updateAvatar/:userID', async(request, reply) => {
+		try {
+
+		} catch (error) {
+			serv.log.error(`Error fetching user profile: ${error}`);
+			return reply.code(500).send({ message: 'Internal server error' });
+		}
 	});
 
 	//update user's bio with userID
 	serv.post('/users/updateBio/:userID', async(request, reply) => {
+		try {
+
+		} catch (error) {
+			serv.log.error(`Error fetching user profile: ${error}`);
+			return reply.code(500).send({ message: 'Internal server error' });
+		}
 	});
 
 	//update user's profile color with userID
 	serv.post('/users/updateProfilColor/:userID', async(request, reply) => {
+		try {
+
+		} catch (error) {
+			serv.log.error(`Error fetching user profile: ${error}`);
+			return reply.code(500).send({ message: 'Internal server error' });
+		}
 	});
 
 	//update user's activity with userID
 	serv.post('/users/updateActivityStatus/:userID', async(request, reply) => {
+		try {
+
+		} catch (error) {
+			serv.log.error(`Error fetching user profile: ${error}`);
+			return reply.code(500).send({ message: 'Internal server error' });
+		}
 	});
 
 	//update user's last connextion with userID
 	serv.post('/users/updateLastConnexion/:userID', async(request, reply) => {
+		try {
+
+		} catch (error) {
+			serv.log.error(`Error fetching user profile: ${error}`);
+			return reply.code(500).send({ message: 'Internal server error' });
+		}
 	});
 
 	//update user's username with userID
 	serv.post('/users/updateUsername/:userID', async(request, reply) => {
+		try {
+
+		} catch (error) {
+			serv.log.error(`Error fetching user profile: ${error}`);
+			return reply.code(500).send({ message: 'Internal server error' });
+		}
 	});
 
 	//USER STATS
 	//get all user's stats with userID
 	serv.get('/users/usersStats/:userID', async(request, reply) => {
+		try {
+
+		} catch (error) {
+			serv.log.error(`Error fetching user profile: ${error}`);
+			return reply.code(500).send({ message: 'Internal server error' });
+		}
 	});
 
 	//update user's longuest match with userID
 	serv.post('/users/updateLonguestMatch/:userID', async(request, reply) => {
+		try {
+
+		} catch (error) {
+			serv.log.error(`Error fetching user profile: ${error}`);
+			return reply.code(500).send({ message: 'Internal server error' });
+		}
 	});
 
 	//update user's shortest match with userID
 	serv.post('/users/updateShortestMatch/:userID', async(request, reply) => {
+		try {
+
+		} catch (error) {
+			serv.log.error(`Error fetching user profile: ${error}`);
+			return reply.code(500).send({ message: 'Internal server error' });
+		}
 	});
 
 	//update user's total match with userID
 	serv.post('/users/updateTotalMatch/:userID', async(request, reply) => {
+		try {
+
+		} catch (error) {
+			serv.log.error(`Error fetching user profile: ${error}`);
+			return reply.code(500).send({ message: 'Internal server error' });
+		}
 	});
 
 	//update user's total wins with userID
 	serv.post('/users/updateWins/:userID', async(request, reply) => {
+		try {
+
+		} catch (error) {
+			serv.log.error(`Error fetching user profile: ${error}`);
+			return reply.code(500).send({ message: 'Internal server error' });
+		}
 	});
 
 	//update user's win streak with userID
 	serv.post('/users/updateWinStreak/:userID', async(request, reply) => {
+		try {
+
+		} catch (error) {
+			serv.log.error(`Error fetching user profile: ${error}`);
+			return reply.code(500).send({ message: 'Internal server error' });
+		}
 	});
 
 	//update user's average match duration with userID
 	serv.post('/users/updateAverageMatchDuration/:userID', async(request, reply) => {
+		try {
+
+		} catch (error) {
+			serv.log.error(`Error fetching user profile: ${error}`);
+			return reply.code(500).send({ message: 'Internal server error' });
+		}
 	});
 
 	//update user's highest score with userID
 	serv.post('/users/updateHighestScore/:userID', async(request, reply) => {
+		try {
+
+		} catch (error) {
+			serv.log.error(`Error fetching user profile: ${error}`);
+			return reply.code(500).send({ message: 'Internal server error' });
+		}
 	});
-
-
 }
