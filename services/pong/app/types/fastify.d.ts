6c0c1c8aeacfedcae8ff96ddf2fcf8ccd5e1c15b
Charlotte Sweeting <charlottesweeting@email>
2025-10-02 15:43:16 +0200

    chore: added tabs

--- services/pong/app/types/fastify.d.ts
+++ services/pong/app/types/fastify.d.ts
@@ -3,8 +3,8 @@ import { GameRegistry } from '../src/classes/gameRegistry.class.js';
 import type { NatsConnection } from 'nats'
 
 declare module 'fastify' {
-  interface FastifyInstance {
-    gameRegistry: GameRegistry;
-    nc: NatsConnection;
-  }
+    interface FastifyInstance {
+      gameRegistry: GameRegistry;
+      nc: NatsConnection;
+    }
 }
