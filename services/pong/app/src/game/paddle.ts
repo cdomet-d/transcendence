5eabd476450f0e057b8b3688527044584c024645
Charlotte Sweeting <charlottesweeting@email>
2025-10-07 16:42:13 +0200

    chore: moved speed in player class will probably move it to game class later on

--- services/pong/app/src/game/paddle.ts
+++ services/pong/app/src/game/paddle.ts
@@ -1,22 +1,20 @@
-import type { paddleObj, Player } from '../classes/player.class.js';
+import type { coordinates, Player } from '../classes/player.class.js';
 import type { keysObj } from './mess.validation.js';
 import { HEIGHT } from '../classes/game.class.js'
 
-const padSpeed: number = 0.2;
-
 export function updatePaddlePos(player: Player, keys: keysObj, delta: number) {
     if ((player.left && keys._w) || (player.right && keys._ArrowUp))
-        up(player.paddle, delta);
+        up(player.paddle, player.speed.paddle, delta);
     if ((player.left && keys._s) || (player.right && keys._ArrowDown))
-        down(player.paddle, delta);
+        down(player.paddle, player.speed.paddle, delta);
 }
 
-function up(pad: paddleObj, delta: number) {
+function up(pad: coordinates, padSpeed: number, delta: number) {
     if (pad.y - (padSpeed * delta) > 0)
         pad.y -= (padSpeed * delta);
 }
 
-function down(pad: paddleObj, delta: number) {
+function down(pad: coordinates, padSpeed: number, delta: number) {
     if (pad.y + (padSpeed * delta) < (HEIGHT - 54))
         pad.y += (padSpeed * delta);
 }
