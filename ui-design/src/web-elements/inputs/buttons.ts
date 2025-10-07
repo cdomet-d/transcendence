d9c2de41ff3f510287744a66b3b226a2e27cb201
Jauppp <coralie.dometdemont@gmail.com>
2025-10-07 17:55:54 +0200

    doc: radio & checkboxes

--- ui-design/src/web-elements/inputs/buttons.ts
+++ ui-design/src/web-elements/inputs/buttons.ts
@@ -1,18 +1,27 @@
+/**
+ * Custom radio button element.
+ * Extends HTMLDivElement.
+ */
 export class radioBtn extends HTMLDivElement {
   input: HTMLInputElement;
   check: HTMLSpanElement;
 
+  /**
+   * Sets the name attribute of the internal radio input.
+   */
   set inputName(val: string) {
     this.input.name = val;
   }
 
+  /**
+   * Sets the id attribute of the internal radio input.
+   */
   set inputId(val: string) {
     this.input.id = val;
   }
 
   constructor() {
     super();
-
     this.input = document.createElement("input");
     this.check = document.createElement("span");
     this.input.type = "radio";
@@ -33,22 +42,33 @@ export class radioBtn extends HTMLDivElement {
       "box-border relative block border-none w-[36px] h-[24px] brdr clear-bg";
   }
 }
+
 customElements.define("radio-btn", radioBtn, { extends: "div" });
 
+/**
+ * Custom checkbox element.
+ * Extends HTMLDivElement.
+ */
 export class checkBox extends HTMLDivElement {
   input: HTMLInputElement;
   check: HTMLSpanElement;
 
+  /**
+   * Sets the name attribute of the internal checkbox input.
+   */
   set inputName(val: string) {
     this.input.name = val;
   }
+
+  /**
+   * Sets the id attribute of the internal checkbox input.
+   */
   set inputId(val: string) {
     this.input.id = val;
   }
 
   constructor() {
     super();
-
     this.input = document.createElement("input");
     this.check = document.createElement("span");
     this.input.type = "checkbox";
@@ -56,6 +76,7 @@ export class checkBox extends HTMLDivElement {
       "peer m-0 z-2 cursor-pointer absolute top-[0] left-[0]";
     this.check.className =
       "peer-checked:block z-1 hidden absolute top-[4px] left-[4px] cursor-pointer w-[16px] h-[16px] thin brdr orange-bg";
+
     this.appendChild(this.input);
     this.appendChild(this.check);
   }
@@ -69,4 +90,5 @@ export class checkBox extends HTMLDivElement {
       "w-[24px] h-[24px] border-box relative block border-none brdr clear-bg";
   }
 }
+
 customElements.define("check-btn", checkBox, { extends: "div" });
