f67e7a1e1825e128df6e0b70873c96853ad5e3fc
Jauppp <coralie.dometdemont@gmail.com>
2025-10-07 17:28:40 +0200

    doc: updated documentation to input button helpers

--- ui-design/src/web-element-helpers/inputs/buttons-helper-modules.ts
+++ ui-design/src/web-element-helpers/inputs/buttons-helper-modules.ts
@@ -1,35 +1,34 @@
-import { checkBox, radioBtn } from "../../web-elements/inputs/buttons";
+import { checkBox, radioBtn } from '../../web-elements/inputs/buttons';
 /**
  * Creates a custom radio button element.
  *
- * @param btnName - name attribute for the internal radio input.
- * @param btnId - id attribute for the internal radio input.
- * @returns A radioBtn custom element as an HTMLDivElement.
+ * @param btnName - The name attribute for the radio input.
+ * @param btnId - The id attribute for the radio input.
+ * @returns A {@link radioBtn} custom element as an HTMLDivElement.
  *
  * @example
  * wrapper.appendChild(createRadioBtn("radio-group", "radio-1"));
  */
 export function createRadioBtn(btnName: string, btnId: string): HTMLDivElement {
-  const el = document.createElement("div", { is: "radio-btn" }) as radioBtn;
-  el.inputId = btnId;
-  el.inputName = btnName;
-  return el;
+    const el = document.createElement('div', { is: 'radio-btn' }) as radioBtn;
+    el.inputId = btnId;
+    el.inputName = btnName;
+    return el;
 }
 
 /**
  * Creates a custom checkbox element.
  *
- * @param btnName - name attribute for the internal checkbox input.
- * @param btnId - id attribute for the internal checkbox input.
- * @returns A checkBtn custom element as an HTMLDivElement.
+ * @param btnName - The name attribute for the checkbox input.
+ * @param btnId - The id attribute for the checkbox input.
+ * @returns A {@link checkBox} custom element as an HTMLDivElement.
  *
  * @example
  * wrapper.appendChild(createCheckbox("checkbox-group", "checkbox1"));
  */
-
 export function createCheckbox(btnName: string, btnId: string): HTMLDivElement {
-  const el = document.createElement("div", { is: "check-btn" }) as checkBox;
-  el.inputId = btnId;
-  el.inputName = btnName;
-  return el;
+    const el = document.createElement('div', { is: 'check-btn' }) as checkBox;
+    el.inputId = btnId;
+    el.inputName = btnName;
+    return el;
 }
