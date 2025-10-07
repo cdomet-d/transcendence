c4bed91e2f99f4b4e43907244f67e6713ed24ee3
Charlotte Sweeting <charlottesweeting@email>
2025-10-07 16:42:23 +0200

    add: updateScore function

--- services/pong/app/src/game/ball.ts
+++ services/pong/app/src/game/ball.ts
@@ -1,36 +1,48 @@
-import { HEIGHT, WIDTH } from "../classes/game.class.js";
-import type { paddleObj, ballObj } from "../classes/player.class.js";
+import { HEIGHT, WIDTH, type ballObj } from "../classes/game.class.js";
+import type { coordinates, Player } from "../classes/player.class.js";
 
-let dx: number = 0.2;
-let dy: number = 0.05;
+export function updateBallPos(ball: ballObj, player: Player, opponent: Player, delta: number): boolean {
+	let newX: number = ball.x + (ball.dx * delta);
+	let newY: number = ball.y + (ball.dy * delta);
 
-export function updateBallPos(ball: ballObj, leftPad: paddleObj, rightPad: paddleObj, delta: number) {
-    if (ball.x + (dx * delta) > WIDTH || ball.x + (dx * delta) < 0) {
-        ball.x = WIDTH / 2;
-        ball.y = HEIGHT / 2;
-        return;
-    }
+	if (newX - 10 > WIDTH || newX + 10 < 0) {
+		if (updateScore(player, opponent, newX))
+			return true;
+		ball.x = WIDTH / 2;
+		ball.y = HEIGHT / 2;
+		ball.dx *= -1;
+		return false;
+	}
 
-    let newX: number = ball.x + (dx * delta);
-    let newY: number = ball.y + (dy * delta);
+	if ((newY + 10) > HEIGHT || (newY - 10) < 0 ) {
+		ball.dy *= -1;
+		newY = ball.y + (ball.dy * delta);
+	}
+	if (touchesPad(player.paddle, opponent.paddle, newX, newY)) {
+		ball.dx *= -1;
+		newX = ball.x + (ball.dx * delta);
+	}
 
-    if ((newY + 10) > HEIGHT || (newY - 10) < 0 ) {
-        dy *= -1;
-        newY = ball.y + (dy * delta);
-    }
-    if (touchesPad(leftPad, rightPad, newX, newY)) {
-        dx *= -1.1; //TODO: reinitialise
-        newX = ball.x + (dx * delta);
-    }
-
-    ball.x = newX;
-    ball.y = newY;
+	ball.x = newX;
+	ball.y = newY;
+	return false
 }
 
-function touchesPad(leftPad: paddleObj, rightPad: paddleObj, newX: number, newY: number): boolean {
-    if (newX + 10 > rightPad.x && (newY + 10 > rightPad.y && newY - 10 < rightPad.y + 54))
-        return true;
-    if (newX - 10 < leftPad.x && (newY + 1 > leftPad.y && newY - 1 < leftPad.y + 54))
-        return true;
-    return false;
+function touchesPad(leftPad: coordinates, rightPad: coordinates, newX: number, newY: number): boolean {
+	if (newX + 10 > rightPad.x && (newY + 10 > rightPad.y && newY - 10 < rightPad.y + 54))
+		return true;
+	if (newX - 10 < leftPad.x && (newY + 1 > leftPad.y && newY - 1 < leftPad.y + 54))
+		return true;
+	//TODO: check top of paddle
+	return false;
+}
+
+function updateScore(player: Player, opponent: Player, newX: number): boolean {
+	if (newX - 10 > WIDTH)
+		player.incScore();
+	if (newX + 10 < 0)
+		opponent.incScore();
+	if (player.score === 5 || opponent.score === 5)
+		return true;
+	return false;
 }
