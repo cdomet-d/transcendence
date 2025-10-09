12cb4ac9de87d47a09cd8309ba61845dcb5be899
Charlotte Sweeting <charlottesweeting@email>
2025-10-09 17:20:25 +0200

    fix: replaced right player x with fixed value 460 which temporarely fixes remote ball movement

--- services/pong/app/src/game/ball.ts
+++ services/pong/app/src/game/ball.ts
@@ -1,15 +1,18 @@
 import { HEIGHT, WIDTH, type ballObj } from "../classes/game.class.js";
 import type { coordinates, Player } from "../classes/player.class.js";
 
-export function updateBallPos(ball: ballObj, player: Player, opponent: Player, delta: number): boolean {
+export function updateBallPos(ball: ballObj, player1: Player, player2: Player, delta: number): boolean {
 	let newX: number = ball.x + (ball.dx * delta);
 	let newY: number = ball.y + (ball.dy * delta);
 
 	if (newX - 10 > WIDTH || newX + 10 < 0) {
-		if (updateScore(player, opponent, newX))
+		if (updateScore(player1, player2, newX))
 			return true;
 		ball.x = WIDTH / 2;
 		ball.y = HEIGHT / 2;
+		if (ball.dx === ball.lastdx)
+			ball.dx *= -1;
+		ball.lastdx = ball.dx;
 		return false;
 	}
 
@@ -17,7 +20,7 @@ export function updateBallPos(ball: ballObj, player: Player, opponent: Player, d
 		ball.dy *= -1;
 		newY = ball.y + (ball.dy * delta);
 	}
-	if (touchesPad(player.paddle, opponent.paddle, newX, newY)) {
+	if (touchesPad(player1.paddle, player2.paddle, newX, newY)) {
 		ball.dx *= -1;
 		newX = ball.x + (ball.dx * delta);
 	}
@@ -29,24 +32,25 @@ export function updateBallPos(ball: ballObj, player: Player, opponent: Player, d
 
 const maxScore: number = 3;
 
-function updateScore(player: Player, opponent: Player, newX: number): boolean {
+function updateScore(player1: Player, player2: Player, newX: number): boolean {
 	if (newX - 10 > WIDTH) {
-		player.incScore();
-		console.log("PLAYER:", player.score);
+		player1.incScore();
+		console.log("PLAYER:", player1.score);
 	}
 	if (newX + 10 < 0) {
-		opponent.incScore();
-		console.log("OPPONENT:", player.score);
+		player2.incScore();
+		console.log("OPPONENT:", player2.score);
 	}
-	if (player.score === maxScore || opponent.score === maxScore)
+	if (player1.score === maxScore || player2.score === maxScore)
 		return true;
 	return false;
 }
 
 function touchesPad(leftPad: coordinates, rightPad: coordinates, newX: number, newY: number): boolean {
-	if (newX + 10 > rightPad.x && (newY + 10 > rightPad.y && newY - 10 < rightPad.y + 54))
+	//TODO: fix 460
+	if (newX + 10 > 460 && (newY + 10 > rightPad.y && newY - 10 < rightPad.y + 54))
 		return true;
-	if (newX - 10 < leftPad.x + 10 && (newY + 1 > leftPad.y && newY - 1 < leftPad.y + 54))
+	if (newX - 10 < leftPad.x + 10 && (newY + 10 > leftPad.y && newY - 10 < leftPad.y + 54))
 		return true;
 	//TODO: check top of paddle
 	return false;
