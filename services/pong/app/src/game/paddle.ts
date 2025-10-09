cf2c2243f0166ad5605a20455876e23db1b440a5
Charlotte Sweeting <charlottesweeting@email>
2025-10-09 11:41:39 +0200

    fix: passing  padSpeed in function params

--- services/pong/app/src/game/paddle.ts
+++ services/pong/app/src/game/paddle.ts
@@ -2,11 +2,14 @@ import type { coordinates, Player } from '../classes/player.class.js';
 import type { keysObj } from './mess.validation.js';
 import { HEIGHT } from '../classes/game.class.js'
 
-export function updatePaddlePos(player: Player, keys: keysObj, delta: number) {
-    if ((player.left && keys._w) || (player.right && keys._ArrowUp))
-        up(player.paddle, player.speed.paddle, delta);
-    if ((player.left && keys._s) || (player.right && keys._ArrowDown))
-        down(player.paddle, player.speed.paddle, delta);
+export function updatePaddlePos(player: Player, keys: keysObj, padSpeed: number, delta: number) {
+    if ((player.left && keys._w) || (player.right && keys._ArrowUp)) {
+        up(player.paddle, padSpeed, delta);
+    }
+    if ((player.left && keys._s) || (player.right && keys._ArrowDown)) {
+        down(player.paddle, padSpeed, delta);
+    }
+    keys = {_w: false, _s: false, _ArrowUp: false, _ArrowDown: false};
 }
 
 function up(pad: coordinates, padSpeed: number, delta: number) {
