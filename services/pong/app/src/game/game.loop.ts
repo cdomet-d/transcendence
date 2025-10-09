72c57bee73a1a7f453a76617252a4911ea3c6a20
Charlotte Sweeting <charlottesweeting@email>
2025-10-09 11:41:08 +0200

    fix: replaced local and remote with sendToplayer1 and sendtoplayer2

--- services/pong/app/src/game/game.loop.ts
+++ services/pong/app/src/game/game.loop.ts
@@ -1,65 +1,45 @@
-import type { Game } from "../classes/game.class";
+import type { Game } from "../classes/game.class.js";
 import { updateBallPos } from './ball.js';
 import { updatePaddlePos } from './paddle.js';
 import type { ballObj } from '../classes/game.class.js';
+import { coordinates, Player } from "../classes/player.class.js";
 
 const TICK_RATE:number = 1000 / 60;
-
-export function gameLoop(game: Game) {
-    //updatePaddles;
-    //updateBall;
-    //send message to players
-    const timeout: number = 1000 / 60;
-    setTimeout(gameLoop, timeout, game);
-}
-
-/*
 let lastFrameTime: number = 0;
 
-const delta: number = mess._timeStamp - lastFrameTime;
-lastFrameTime = mess._timeStamp;
-sendReply(player, opponent, ball, keys, delta);
-*/
-
-/*
-function local(player: Player, opponent: Player, ball: ballObj, keys: keysObj, delta: number) {
-    updatePaddlePos(player, keys, delta);
-    updatePaddlePos(opponent, keys, delta);
-    if (updateBallPos(ball, player, opponent, delta)) {
-        player.socket.close();
+export function gameLoop(game: Game, player1: Player, player2: Player) {
+    const timestamp: number = Date.now();
+    const delta: number =  timestamp - lastFrameTime;
+    lastFrameTime = timestamp;
+    updatePaddlePos(player1, player1.keys, game.paddleSpeed, delta);
+    updatePaddlePos(player2, player2.keys, game.paddleSpeed, delta);
+    if (updateBallPos(game.ball, player1, player2, delta)) {
+        player1.socket.close();
+        if (player2.socket.readyState === 1)
+            player2.socket.close();
         //TODO: send result to gameManager via nats
         return;
     }
+    sendToPlayer1(player1, player2.paddle, game.ball);
+    if (!game.local)
+        sendToPlayer2(player2, player1.paddle, game.ball);
+    // const timeout: number = 1000 / 60;
+    setTimeout(gameLoop, TICK_RATE, game, player1, player2);
+}
+
+function sendToPlayer1(player: Player, opponentPaddle: coordinates, ball: ballObj) {
     player.setMessPad("left", player.paddle.y);
-    player.setMessPad("right", opponent.paddle.y);
+    player.setMessPad("right", opponentPaddle.y);
     player.setMessBall("left", ball);
-    if (player.socket.readyState === 1)
+    if (player.socket.readyState === 1) {
         player.socket.send(JSON.stringify(player.rep));
-    //TODO: handle case where socket isn't open
-}
-*/
-
-/*
-function remote(player: Player, opponent: Player, ball: ballObj, keys: keysObj, delta: number) {
-    updatePaddlePos(player, keys, delta);
-    player.setMessPad("left", player.paddle.y);
-    opponent.setMessPad("right", player.paddle.y);
-    
-    if (player.ballMaster) { //TODO: change ballMaster if player gets disconnected
-        if (updateBallPos(ball, player, opponent, delta)) {
-            player.socket.close();
-            opponent.socket.close();
-            //TODO: send result to gameManager via nats
-            return;
-        }
-        player.setMessBall("left", ball);
-        opponent.setMessBall("right", ball);
     }
-    
-    if (opponent.socket.readyState === 1)
-        opponent.socket.send(JSON.stringify(opponent.rep));
+}
+
+function sendToPlayer2(player: Player, opponentPaddle: coordinates, ball: ballObj) {
+    player.setMessPad("left", player.paddle.y);
+    player.setMessPad("right", opponentPaddle.y);
+    player.setMessBall("right", ball);
     if (player.socket.readyState === 1)
         player.socket.send(JSON.stringify(player.rep));
 }
-
-*/
