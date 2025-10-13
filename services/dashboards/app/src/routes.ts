07cfa04ad7a72bc71e47488f0392174062e8d868
Eiixela <alexielaurine@live.fr>
2025-10-13 12:30:56 +0200

    add: to do for dashboard routes

--- services/dashboards/app/src/routes.ts
+++ services/dashboards/app/src/routes.ts
@@ -1,1 +1,20 @@
 import type { FastifyInstance } from 'fastify';
+
+export async function dashboardRoute(serv: FastifyInstance) {
+
+	//post a game
+	//post a tournament
+	//patch a winner game
+	//patch a winner tournament
+	//delete a player from a game ?
+	//delete a game
+	//delete a tournamement
+	//patch duration
+	//patch score
+	//patch status
+	//get all game of a player
+	//get all tournamenet of a player
+	//get all game of a tournamenent
+	//get stats of a game
+	
+}
--- services/dashboards/app/src/serv.d.ts
+++ services/dashboards/app/src/serv.d.ts
@@ -3,5 +3,9 @@ import { Database } from 'sqlite';
 declare module 'fastify' {
   export interface FastifyInstance {
     dbStats: Database;
+    user : {
+      userID : number,
+      username : string
+    };
   }
 }
