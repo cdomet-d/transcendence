3dc9af503f666d907c7895040b82c22b1fa4d07b
Charlotte Sweeting <charlottesweeting@email>
2025-10-15 10:47:53 +0200

    rm: lastServerState

--- services/frontend/app/src/client/scripts/game/game.loop.ts
+++ services/frontend/app/src/client/scripts/game/game.loop.ts
@@ -1,6 +1,6 @@
 import { createKeyDownEvent, createKeyUpEvent, addMessEvent } from "./game.events.js";
 import { renderGame } from "./game.render.utils.js";
-import { Game, HEIGHT, WIDTH, type serverReplyObj } from "./game.class.js";
+import { Game, HEIGHT, WIDTH } from "./game.class.js";
 import { updatePaddlePos } from "./paddle.js";
 import { updateBallPos } from "./ball.js";
 
@@ -10,33 +10,38 @@ export function startGame(game: Game, ws: WebSocket) {
 	addMessEvent(game, ws);
 	window.addEventListener("keydown", createKeyDownEvent(game.req._keys));
 	window.addEventListener("keyup", createKeyUpEvent(game.req._keys));
-	const lastServerState: serverReplyObj = { ...game.servReply };
 	ws.send("1");
 	game.frameId = requestAnimationFrame((timestamp) => {
 		game.lastFrameTime = timestamp;
-		game.frameId = requestAnimationFrame(FrameRequestCallback(game, lastServerState, ws));
+		game.frameId = requestAnimationFrame(FrameRequestCallback(game, ws));
 	});
 }
 
-function FrameRequestCallback(game: Game, lastServerState: serverReplyObj, ws: WebSocket) {
+function FrameRequestCallback(game: Game, ws: WebSocket) {
 	return function gameLoop(timestamp: number) {
+		//reconciliation
 		if (game.reqHistory.has(game.servReply._ID))
 			reconciliation(game);
-		ws.send(JSON.stringify(game.req));
-		game.addReq(game.req);
-		game.req._ID += 1; //TODO: overflow
-		// game.req._timeStamp = timestamp;
+
+		//prediction
 		game.delta += (timestamp - game.lastFrameTime);
 		game.lastFrameTime = timestamp;
-		while (game.delta >= TIME_STEP) { //prediction
+		while (game.delta >= TIME_STEP) {
 			updatePaddlePos(game, game.req._keys, TIME_STEP);
 			updateBallPos(game, TIME_STEP);
 			game.delta -= TIME_STEP;
 		}
-		lastServerState = { ...game.servReply };
+
+		//req to server
+		ws.send(JSON.stringify(game.req));
+		game.addReq(game.req);
+		game.req._ID += 1; //TODO: overflow
+		// game.req._timeStamp = timestamp;
+
+		//new frame
 		game.ctx.clearRect(0, 0, WIDTH, HEIGHT);
 		renderGame(game);
-		game.frameId = window.requestAnimationFrame(FrameRequestCallback(game, lastServerState, ws));
+		game.frameId = window.requestAnimationFrame(FrameRequestCallback(game, ws));
 	}
 }
 
@@ -49,17 +54,8 @@ function reconciliation(game: Game) {
 	game.ball.x = game.servReply._ball.x;
 	game.ball.y = game.servReply._ball.y;
 
-	let timestamp: number;
-	let lastTimeStamp: number = Date.now();
-	let delta: number = 0;
 	for (let i = id + 1; game.reqHistory.has(i); i++) {
-		// timestamp = Date.now();
-		// delta += (timestamp - lastTimeStamp);
-		// lastTimeStamp = timestamp;
-		// while (delta >= TIME_STEP) {
-			updatePaddlePos(game, game.reqHistory.get(i)!._keys, TIME_STEP)
-			updateBallPos(game, TIME_STEP);
-		// 	delta -= TIME_STEP;
-		// }
+		updatePaddlePos(game, game.reqHistory.get(i)!._keys, TIME_STEP)
+		updateBallPos(game, TIME_STEP);
 	}
 }
