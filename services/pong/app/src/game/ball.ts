3307120b2febd9cd72366379cc346fd7938eb4c8
Charlotte Sweeting <charlottesweeting@email>
2025-10-24 14:12:43 +0200

    fix: used ballDir to choose ball direction after point is scored

--- services/pong/app/src/game/ball.ts
+++ services/pong/app/src/game/ball.ts
@@ -1,45 +1,39 @@
-import { HEIGHT, WIDTH, type ballObj } from "../classes/game.class.js";
+import { Game, HEIGHT, WIDTH, type ballObj } from "../classes/game.class.js";
 import type { coordinates, Player } from "../classes/player.class.js";
 
 const TIME_STEP: number = 1000 / 60; // 60FPS
 const MAX_SCORE: number = 5;
 
-export function updateBallPos(ball: ballObj, player1: Player, player2: Player): boolean {
-	let newX: number = ball.x + (ball.dx * TIME_STEP);
-	let newY: number = ball.y + (ball.dy * TIME_STEP);
+export function updateBallPos(game: Game, player1: Player, player2: Player): boolean {
+	let newX: number = game.ball.x + (game.ball.dx * TIME_STEP);
+	let newY: number = game.ball.y + (game.ball.dy * TIME_STEP);
 
 	if (newX - 10 >= WIDTH || newX + 10 <= 0) {
 		if (updateScore(player1, player2, newX))
 			return true;
-		ball.x = WIDTH / 2;
-		ball.y = HEIGHT / 2;
-		// if (ball.dx === ball.lastdx)
-		// 	ball.dx *= -1;
-		// if (ball.dy < 0)
-		// 	ball.dy *= -1;
-		// ball.lastdx = ball.dx;
-		ball.dx = 0;
-		ball.lastdx = 0;
-		ball.dy = 0;
+		game.ball.x = WIDTH / 2;
+		game.ball.y = HEIGHT / 2;
+		game.ball.dx = 0;
+		game.ball.dy = 0;
 		setTimeout(() => {
-			ball.dx = 0.3;
-			ball.lastdx = 0.3;
-			ball.dy = 0.025;
+			game.ball.dx = 0.3 * game.ballDir;
+			game.ball.dy = 0.025;
+			game.ballDir *= -1;
 		}, 1000);
 		return false;
 	}
 
 	if ((newY + 10) >= HEIGHT || (newY - 10) <= 0 ) {
-		ball.dy *= -1;
-		newY = ball.y + (ball.dy * TIME_STEP);
+		game.ball.dy *= -1;
+		newY = game.ball.y + (game.ball.dy * TIME_STEP);
 	}
 	if (paddleCollision(player1.paddle, player2.paddle, newX, newY)) {
-		ball.dx *= -1;
-		newX = ball.x + (ball.dx * TIME_STEP);
+		game.ball.dx *= -1;
+		newX = game.ball.x + (game.ball.dx * TIME_STEP);
 	}
 
-	ball.x = newX;
-	ball.y = newY;
+	game.ball.x = newX;
+	game.ball.y = newY;
 	return false
 }
 
