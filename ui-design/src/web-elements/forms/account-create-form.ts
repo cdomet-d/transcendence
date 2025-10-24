da416dde7dcbaa365a08ae9a28747f07260c94c3
Jauppp <coralie.dometdemont@gmail.com>
2025-10-24 18:03:08 +0200

    chore: format

--- ui-design/src/web-elements/forms/account-create-form.ts
+++ ui-design/src/web-elements/forms/account-create-form.ts
@@ -1,8 +1,6 @@
 import { BaseForm } from './baseform';
 
-export class AccountCreationForm extends BaseForm {
-	
-}
+export class AccountCreationForm extends BaseForm {}
 
 if (!customElements.get('account-creation-form')) {
     customElements.define('account-creation-form', AccountCreationForm, { extends: 'form' });
