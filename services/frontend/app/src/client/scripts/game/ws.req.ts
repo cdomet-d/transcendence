d8fd4d70757d781a002cd8857cdf083e78d9947a
Charlotte Sweeting <charlottesweeting@email>
2025-11-03 09:29:00 +0100

    chore: switched spacee with tabs

--- services/frontend/app/src/client/scripts/game/ws.req.ts
+++ services/frontend/app/src/client/scripts/game/ws.req.ts
@@ -5,44 +5,44 @@ import { createKeyDownEvent, createKeyUpEvent, addMessEvent } from "./game.event
 import { syncClocks } from './syncClocks.js';
 
 export function wsRequest(game: Game) {
-    const ws = new WebSocket('wss://localhost:8443/api/game/match'); //?gameID=1');
+	const ws = new WebSocket('wss://localhost:8443/api/game/match'); //?gameID=1');
 
-    ws.onerror = (err) => {
-        console.log("error:", err);
-        return; //TODO: handle error properly
-    }
+	ws.onerror = (err) => {
+		console.log("error:", err);
+		return; //TODO: handle error properly
+	}
 
-    ws.onopen = () => {
-        console.log("WebSocket connection established!")
-        ws.addEventListener('message', (event) => {
-            const signal: number = JSON.parse(event.data);
-            // console.log("SIGNAL:", signal, "TYPE", typeof(signal));
-            if (signal === 1)
-                setUpGame(game, ws);
-        }, { once: true });
-        ws.send(JSON.stringify({gameID: 1, userID: 1})); //TODO: only for testing
-    }
+	ws.onopen = () => {
+		console.log("WebSocket connection established!")
+		ws.addEventListener('message', (event) => {
+			const signal: number = JSON.parse(event.data);
+			// console.log("SIGNAL:", signal, "TYPE", typeof(signal));
+			if (signal === 1)
+				setUpGame(game, ws);
+		}, { once: true });
+		ws.send(JSON.stringify({gameID: 1, userID: 1})); //TODO: only for testing
+	}
 
-    ws.onclose = () => {
-        window.cancelAnimationFrame(game.frameId);
-    }
+	ws.onclose = () => {
+		window.cancelAnimationFrame(game.frameId);
+	}
 }
 
 export async function setUpGame(game: Game, ws: WebSocket) {
-    addMessEvent(game, ws);
+	addMessEvent(game, ws);
 
-    const result: [number, number, startObj] | null = await syncClocks(ws)
-    if (!result) return;
-    const [offset, halfTripTime, start] = result;
-    game.clockOffset = offset;
-    game.ball.dx *= start.ballDir;
-    window.addEventListener("keydown", createKeyDownEvent(game.req._keys));
-    window.addEventListener("keyup", createKeyUpEvent(game.req._keys));
+	const result: [number, number, startObj] | null = await syncClocks(ws)
+	if (!result) return;
+	const [offset, halfTripTime, start] = result;
+	game.clockOffset = offset;
+	game.ball.dx *= start.ballDir;
+	window.addEventListener("keydown", createKeyDownEvent(game.req._keys));
+	window.addEventListener("keyup", createKeyUpEvent(game.req._keys));
 
-    // wait
-    const waitTime = Math.max(0, start.delay - halfTripTime);
-    await new Promise(res => setTimeout(res, waitTime));
+	// wait
+	const waitTime = Math.max(0, start.delay - halfTripTime);
+	await new Promise(res => setTimeout(res, waitTime));
 
-    //start game
-    startGame(game, ws);
+	//start game
+	startGame(game, ws);
 }
