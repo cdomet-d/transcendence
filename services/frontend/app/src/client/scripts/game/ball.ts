69fa5dfaf0b7712b6fbb38f436514188aa505461
Charlotte Sweeting <charlottesweeting@email>
2025-11-07 15:25:29 +0100

    fix: only using line intersection for collision but doesn't work yet

--- services/frontend/app/src/client/scripts/game/ball.ts
+++ services/frontend/app/src/client/scripts/game/ball.ts
@@ -7,7 +7,7 @@ export function deadReckoning(game: Game, latestReply: repObj | undefined) {
 	let ball: ballObj = { ...game.ball };
 	if (latestReply !== undefined) {
 		timeSinceUpdate = (performance.now() - latestReply._timestamp)
-		ball = latestReply._ball;
+		ball = { ...latestReply._ball };
 	}
 	if (timeSinceUpdate > 100) //TODO: add var for 100
 		timeSinceUpdate = 100;
@@ -43,126 +43,95 @@ function sideWallCollision(game: Game, newX: number): boolean {
 function upperAndBottomWallCollision(game: Game, newY: number): number {
 	if (newY - game.ball.radius <= 0) {
 		newY = game.ball.radius + 1;
-		game.ball.dy = incVelocity(game.ball.dx, Math.abs(game.ball.dy))[1];
+		game.ball.dy = updateVelocity(game.ball.dx, Math.abs(game.ball.dy))[1];
 	}
 	if (newY + game.ball.radius >= HEIGHT) {
 		newY = HEIGHT - (game.ball.radius + 1);
-		game.ball.dy = incVelocity(game.ball.dx, -Math.abs(game.ball.dy))[1];
+		game.ball.dy = updateVelocity(game.ball.dx, -Math.abs(game.ball.dy))[1];
 	}
 	return newY;
 }
 
 export function leftPadCollision(game: Game, leftPad: coordinates, newX: number, newY: number): boolean {
-	const ball: coordinates = {
-		x: newX - (leftPad.x + game.padSpec.halfWidth), 
-		y: newY - (leftPad.y + game.padSpec.halfHeight)
-	};
-	const paddle: coordinates = {
-		x: game.padSpec.halfWidth, 
-		y: game.padSpec.halfHeight
-	};
-	if (distBallPad(ball, paddle) <= game.ball.radius) {
-		[game.ball.x, game.ball.y] = getPosition(game, leftPad, newX, newY);
-		return true;
-	}
-	return false;
+	return getPosition(game, leftPad, newX, newY);
 }
 
 export function rightPadCollision(game: Game, rightPad: coordinates, newX: number, newY: number): boolean {
-	const ball: coordinates = {
-		x: newX - (rightPad.x + game.padSpec.halfWidth), 
-		y: newY - (rightPad.y + game.padSpec.halfHeight)
-	};
-	const paddle: coordinates = {
-		x: game.padSpec.halfWidth, 
-		y: game.padSpec.halfHeight
-	};
-	if (distBallPad(ball, paddle) <= game.ball.radius) {
-		[game.ball.x, game.ball.y] = getPosition(game, rightPad, newX, newY);
-		return true
-	}
-	return false;
+	return getPosition(game, rightPad, newX, newY);
 }
 
-function distBallPad(p: coordinates, b: coordinates): number {
-    const d: coordinates = {x: Math.abs(p.x) - b.x, y: Math.abs(p.y) - b.y};
-	d.x = Math.max(d.x, 0.0);
-	d.y = Math.max(d.y, 0.0);
-	const length: number = Math.sqrt(d.x * d.x + d.y * d.y);
-    return length + Math.min(Math.max(d.x, d.y), 0.0);
-}
-
-function getPosition(game: Game, paddle: coordinates, newX: number, newY: number): [number, number] {
-	// console.log("IN GETPOSITION")
-	const side: string = getCollisionSide(game, paddle);
+function getPosition(game: Game, paddle: coordinates, newX: number, newY: number): boolean {
 	const offset: number = game.ball.radius + 1;
+	const p1: coordinates = {x: game.ball.x, y: game.ball.y};
+	const p2: coordinates = {x: newX, y: newY};
 	const incPaddle: coordinates = {x: paddle.x - game.ball.radius, y: paddle.y - game.ball.radius};
 	const padHeight: number = game.padSpec.height + 2 * game.ball.radius;
 	const padWidth: number = game.padSpec.width + 2 * game.ball.radius;
+	const side: string = getCollisionSide(game, incPaddle, padHeight, padWidth);
+	let res: [number, number] | null;
 
 	switch (side) {
 		case "left":
-			[newX, newY] = lineIntersection({x: game.ball.x, y: game.ball.y}, {x: newX, y: newY}, incPaddle, {x: incPaddle.x, y: incPaddle.y + padHeight});
-			[game.ball.dx, game.ball.dy] = incVelocity(-Math.abs(game.ball.dx), game.ball.dy);
-			return [newX, newY];
+			res = lineIntersection(p1, p2, incPaddle, {x: incPaddle.x, y: incPaddle.y + padHeight});
+			if (!res) return false;
+			[game.ball.x, game.ball.y] = [res[0] - 1, res[1]];
+			[game.ball.dx, game.ball.dy] = updateVelocity(-Math.abs(game.ball.dx), game.ball.dy);
+			return true;
 		case "right":
-			[newX, newY] = lineIntersection({x: game.ball.x, y: game.ball.y}, {x: newX, y: newY}, {x: incPaddle.x + padWidth, y: incPaddle.y}, {x: incPaddle.x + padWidth, y: incPaddle.y + padHeight});
-			[game.ball.dx, game.ball.dy] = incVelocity(Math.abs(game.ball.dx), game.ball.dy);
-			return [newX, newY];
+			res = lineIntersection(p1, p2, {x: incPaddle.x + padWidth, y: incPaddle.y}, {x: incPaddle.x + padWidth, y: incPaddle.y + padHeight});
+			if (!res) return false;
+			[game.ball.x, game.ball.y] = [res[0] + 1, res[1]];
+			[game.ball.dx, game.ball.dy] = updateVelocity(Math.abs(game.ball.dx), game.ball.dy);
+			return true;
 		case "top":
-			[newX, newY] = lineIntersection({x: game.ball.x, y: game.ball.y}, {x: newX, y: newY}, incPaddle, {x: incPaddle.x + padWidth, y: incPaddle.y});
-			[game.ball.dx, game.ball.dy] = incVelocity(game.ball.dx, -Math.abs(game.ball.dy));
-			return [newX, newY];
+			res = lineIntersection(p1, p2, incPaddle, {x: incPaddle.x + padWidth, y: incPaddle.y});
+			if (!res) return false;
+			[game.ball.x, game.ball.y] = [res[0], res[1] - 1];
+			[game.ball.dx, game.ball.dy] = updateVelocity(game.ball.dx, -Math.abs(game.ball.dy));
+			return true;
 		case "bottom":
-			[newX, newY] = lineIntersection({x: game.ball.x, y: game.ball.y}, {x: newX, y: newY}, {x: incPaddle.x, y: incPaddle.y + padHeight}, {x: incPaddle.x + padWidth, y: incPaddle.y + padHeight});
-			[game.ball.dx, game.ball.dy] = incVelocity(game.ball.dx, Math.abs(game.ball.dy));
-			return [newX, newY];
-		case "topleft":
-			[game.ball.dx, game.ball.dy] = incVelocity(game.ball.dx * -1, game.ball.dy * -1);
-			return [paddle.x - offset, paddle.y - offset];
-		case "topright":
-			[game.ball.dx, game.ball.dy] = incVelocity(game.ball.dx * -1, game.ball.dy * -1);
-			return [paddle.x + game.padSpec.width + offset, paddle.y - offset];
-		case "bottomleft":
-			[game.ball.dx, game.ball.dy] = incVelocity(game.ball.dx * -1, game.ball.dy * -1);
-			return [paddle.x - offset, paddle.y + game.padSpec.height + offset];
-		case "bottomright":
-			[game.ball.dx, game.ball.dy] = incVelocity(game.ball.dx * -1, game.ball.dy * -1);
-			return [paddle.x + game.padSpec.width + offset, paddle.y + game.padSpec.height + offset];
+			res = lineIntersection(p1, p2, {x: incPaddle.x, y: incPaddle.y + padHeight}, {x: incPaddle.x + padWidth, y: incPaddle.y + padHeight});
+			if (!res) return false;
+			[game.ball.x, game.ball.y] = [res[0], res[1] - 1];
+			[game.ball.dx, game.ball.dy] = updateVelocity(game.ball.dx, Math.abs(game.ball.dy));
+			return true;
 		default:
-			return [newX, newY];
+			return false;
 	}
 }
 
-function lineIntersection(p1: coordinates, p2: coordinates, p3: coordinates, p4: coordinates): [number, number] {
+function lineIntersection(p1: coordinates, p2: coordinates, p3: coordinates, p4: coordinates): [number, number] | null {
 	const denom: number = (p4.y - p3.y) * (p2.x - p1.x) - (p4.x - p3.x) * (p2.y - p1.y);
+	if (denom === 0) return null;
 	const t: number = ((p4.x - p3.x) * (p1.y - p3.y) - (p4.y - p3.y) * (p1.x - p3.x)) / denom;
-	// const u: number = ((p2.x - p1.x) * (p1.y - p3.y) - (p2.y - p1.y) * (p1.x - p3.x)) / denom;
+	if ( t > 1 || t < 0) return null;
+	const u: number = ((p2.x - p1.x) * (p1.y - p3.y) - (p2.y - p1.y) * (p1.x - p3.x)) / denom;
+	if (u > 1 || u < 0) return null;
 	const newX: number = p1.x + t * (p2.x - p1.x);
 	const newY: number = p1.y + t * (p2.y - p1.y);
 	return [newX, newY];
 }
 
-function getCollisionSide(game: Game, paddle: coordinates): string {
-    const padCenterX = paddle.x + game.padSpec.halfWidth;
-    const padCenterY = paddle.y + game.padSpec.halfHeight;
+function getCollisionSide(game: Game, paddle: coordinates, padHeight: number, padWidth: number): string {
+    const padCenterX = paddle.x + padWidth / 2;
+    const padCenterY = paddle.y + padHeight / 2;
 
     const dx = game.ball.x - padCenterX;
     const dy = game.ball.y - padCenterY;
 
-    const normalizedX = dx / game.padSpec.halfWidth;
-    const normalizedY = dy / game.padSpec.halfHeight;
-    
+    const normalizedX = dx / (padWidth / 2);
+    const normalizedY = dy / (padHeight / 2);
+
     const absX = Math.abs(normalizedX);
     const absY = Math.abs(normalizedY);
-    
+
     const cornerThreshold = 0.1;
 
     if (Math.abs(absX - absY) < cornerThreshold) {
         if (dx > 0) 
-			return dy < 0 ? "topright" : "bottomright";
+			return "right";
         else
-			return dy < 0 ? "topleft" : "bottomleft";
+			return "left";
     }
 	if (absX > absY)
         return dx > 0 ? "right" : "left";
@@ -170,7 +139,7 @@ function getCollisionSide(game: Game, paddle: coordinates): string {
         return dy > 0 ? "bottom" : "top";
 }
 
-function incVelocity(dx: number, dy: number): [number, number] {
+function updateVelocity(dx: number, dy: number): [number, number] {
 	if (dx <= 0.5 && dx >= -0.5)
 		dx *= 1.2;
 	if (dy <= 0.5 && dy >= -0.5)
