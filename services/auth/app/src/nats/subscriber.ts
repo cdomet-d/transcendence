da56a88ad3eb55da3c4d1c96465beed9bc569a9a
Samuel Perrier <saperrie@z1r3p1.42lyon.fr>
2025-11-11 12:08:50 +0100

    add: SECRET >>> JWT_SECRET
    rm: NATS and moved back server files out of server folder

--- services/auth/app/src/nats/subscriber.ts
+++ services/auth/app/src/nats/subscriber.ts
@@ -1,36 +1,24 @@
-import { connect, StringCodec, type NatsConnection } from 'nats';
-import { natsPublish } from './publisher.js'
-import type { FastifyInstance } from 'fastify';
+// import { connect, StringCodec, type NatsConnection } from 'nats';
+// import type { FastifyInstance } from 'fastify';
 
-export async function initNatsConnection(): Promise<NatsConnection> {
-	let token: string | undefined = process.env.NATS_SERVER_TOKEN;
-	if (!token)
-		throw new Error("NATS token undefined");
-	const nc = await connect({ servers: "nats://nats-server:4222", token: token });
-	return (nc);
-}
+// export async function initNatsConnection(): Promise<NatsConnection> {
+// 	let token: string | undefined = process.env.NATS_SERVER_TOKEN;
+// 	if (!token)
+// 		throw new Error("NATS token undefined");
+// 	const nc = await connect({ servers: "nats://nats-server:4222", token: token });
+// 	return (nc);
+// }
 
-export async function natsSubscription(serv: FastifyInstance) {
-	const sc = StringCodec();
+// export async function natsSubscription(serv: FastifyInstance) {
+// 	const sc = StringCodec();
 
-	const nc = serv.nc;
-	const sub = nc.subscribe('auth.login');
-	// console.log(`Listening for messages on "auth.login"...`);
+// 	const nc = serv.nc;
+// 	const sub = nc.subscribe('auth.login');
+// 	// console.log(`Listening for messages on "auth.login"...`);
 
-	(async () => {
-		for await (const msg of sub) {
-
-			const _gameInfo = JSON.parse(sc.decode(msg.data));
-			// console.log(`Received message: ${JSON.stringify(_gameInfo)}`);
-
-			// Approval given HERE from PONG if game is ok to start
-			if (msg.reply) {
-				const gameReply = {
-					event: "approved",
-					game: _gameInfo
-				}
-				natsPublish(msg.reply, JSON.stringify(gameReply));
-			}
-		}
-	})();
-};
+// 	(async () => {
+// 		for await (const msg of sub) {
+// 		}
+// 	}
+// 	)();
+// };
--- services/auth/app/src/server.ts
+++ services/auth/app/src/server.ts
@@ -6,17 +6,12 @@ import type { FastifyInstance } from 'fastify';
 import cookie from '@fastify/cookie';
 // Local modules
 import { options } from './serv.conf.js'
-import { initNatsConnection, natsSubscription } from '../nats/subscriber.js';
-import { healthcheck, userRoutes } from '../authentication.js';
-
-export var _serv: FastifyInstance;
+// import { initNatsConnection, natsSubscription } from './nats/subscriber.js';
 
 (async () => {
 	try {
 		const serv = await init();
 		await runServ(serv);
-		_serv = serv; // is this bad or?
-		healthcheck(serv);
 	} catch (err) {
 		console.error('server', err);
 		process.exit(1);
@@ -28,9 +23,9 @@ export async function init(): Promise<FastifyInstance> {
 	const serv: FastifyInstance = Fastify(options);
 
 	//nats
-	const nc = await initNatsConnection();
-	serv.decorate("nc", nc);
-	await natsSubscription(serv);
+	// const nc = await initNatsConnection();
+	// serv.decorate("nc", nc);
+	// await natsSubscription(serv);
 
 	//plugins
 	addPlugins(serv);
@@ -42,7 +37,6 @@ export async function init(): Promise<FastifyInstance> {
 //add plugins
 function addPlugins(serv: FastifyInstance) {
 	serv.register(cookie);
-	serv.register(userRoutes, { prefix: '/api/users' });
 }
 
 //run server
@@ -60,24 +54,3 @@ function getPort(): number {
 	return port;
 }
 
-// // // // // // // // // // // // // // // // // // // // // // // // // // // //
-(() => {
-	const payload: object = { userID: "123", role: "Player", exp: 60 * 60 };
-	const secretKey = process.env.SECRET;
-	if (!secretKey) {
-		console.log("Error: could not retrieve secret from .env");
-		return;
-	}
-	const token = jwt.sign(payload, secretKey);
-
-	console.log("REAL:", token);
-})();
-
-// // // // // // // // // // // // // // // // // // // // // // // // // // // //
-const listeners = ['SIGINT', 'SIGTERM']
-listeners.forEach((signal) => {
-	process.on(signal, async () => {
-		await _serv.close()
-		process.exit(0)
-	})
-})
