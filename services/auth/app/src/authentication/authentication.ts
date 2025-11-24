499759c1c2cdc61fe8369ce68d7cd22440407b83
Coralie Domet de Mont <cdomet-d@student.42lyon.fr>
2025-11-24 15:04:21 +0100

    fix: login route after merge

--- services/auth/app/src/authentication/authentication.ts
+++ services/auth/app/src/authentication/authentication.ts
@@ -51,7 +51,7 @@ export async function authenticationRoutes(serv: FastifyInstance) {
         }
     });
 
-	serv.post('/login/', { schema: authSchema }, async (request, reply) => {
+	serv.post('/login', { schema: authSchema }, async (request, reply) => {
 		try {
 			const { username, password } = request.body as { username: string, password: string };
 
