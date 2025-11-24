1547803cf27ba4ec8d239d8569b84702e1bb97cb
Charlotte Sweeting <charlottesweeting@gmail.com>
2025-11-24 18:01:36 +0100

    rm: reply validation cause server isn't gonna send cursed data

--- services/frontend/app/src/client/pong/game.events.ts
+++ services/frontend/app/src/client/pong/game.events.ts
@@ -1,14 +1,9 @@
 import { Game } from './classes/game.class.js';
-import { validRep } from './mess.validation.js';
 import type { keysObj, repObj } from './classes/game.interfaces.js';
 
 export function addMessEvent(game: Game, ws: WebSocket) {
     ws.onmessage = (event) => {
         const rep: repObj = JSON.parse(event.data);
-        // if (!validRep(rep)) {
-        // 	console.log("Error: invalid reply:", JSON.stringify(rep));
-        // 	return
-        // } //TODO: fix validRep / or delete it ?
         rep._timestamp = performance.now();
         game.addReply(rep);
     };
@@ -16,9 +11,9 @@ export function addMessEvent(game: Game, ws: WebSocket) {
 
 export function createKeyDownEvent(keys: keysObj, horizontal: boolean) {
     return function keyDownEvent(event: KeyboardEvent): void {
-        if (event.key === 'z') keys._w = true;
+        if (event.key === 'w') keys._w = true;
         if (event.key === 's') keys._s = true;
-        if (horizontal && event.key === 'q') keys._a = true;
+        if (horizontal && event.key === 'a') keys._a = true;
         if (horizontal && event.key === 'd') keys._d = true;
         if (event.key === 'ArrowUp') {
             event.preventDefault();
@@ -36,9 +31,9 @@ export function createKeyDownEvent(keys: keysObj, horizontal: boolean) {
 export function createKeyUpEvent(keys: keysObj) {
     return function keyUpEvent(event: KeyboardEvent): void {
         event.preventDefault();
-        if (event.key === 'z') keys._w = false;
+        if (event.key === 'w') keys._w = false;
         if (event.key === 's') keys._s = false;
-        if (event.key === 'q') keys._a = false;
+        if (event.key === 'a') keys._a = false;
         if (event.key === 'd') keys._d = false;
         if (event.key === 'ArrowUp') keys._ArrowUp = false;
         if (event.key === 'ArrowDown') keys._ArrowDown = false;
