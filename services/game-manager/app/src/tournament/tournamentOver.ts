b8a2924ad9e34441bdd04138cfe0d85e49bd9db2
Charlotte Sweeting <charlottesweeting@email>
2025-11-28 11:24:33 +0100

    fix: sending only winner ID to dashboard instead of whole tournament obj

--- services/game-manager/app/src/tournament/tournamentOver.ts
+++ services/game-manager/app/src/tournament/tournamentOver.ts
@@ -2,34 +2,25 @@ import { fetch, Agent } from 'undici';
 import type { tournament } from '../manager.interface.js';
 
 export function tournamentOver(tournament: tournament) {
-    // const tournament: tournament = JSON.parse(payload);
-    patchTournamentToDashboard(tournament);
-    // showTournamentWinnerScreen();
+	patchTournamentToDashboard(tournament);
+	// showTournamentWinnerScreen();
 }
 
-const sslAgent = new Agent({
-    connect: { rejectUnauthorized: false }
-});
-
 async function patchTournamentToDashboard(tournament: tournament) {
-    const url = `https://dashboard:1515/tournament/:tournamentID}`;
-    let response: Response;
-
-    try {
-        response = await fetch(url, {
-            method: 'PATCH',
-            dispatcher: sslAgent,
-            body: JSON.stringify(tournament),
-        });
-    } catch (error) {
-        console.error(`[GM] Dashboard service (via route /tournament/:tournamentID) is unreachable: ${error}`);
-        throw new Error('Dashboard service is unreachable.');
-    }
-
-    // TODO check response status ?
-
-    if (!response.ok) {
-        console.error(`[GM] Dashboard service failed with status: ${response.status}`);
-        throw new Error(`Dashboard service failed with status ${response.status}`);
-    }
+	const url = `http://dashboard:1515/tournament/:${tournament.tournamentID}`;
+	try {
+		const response: Response = await fetch(url, {
+			method: 'PATCH',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify(tournament.winnerID),
+		});
+		// TODO check response status ?	
+		if (!response.ok) {
+			console.error(`[GM] Dashboard service failed with status: ${response.status}`);
+			throw new Error(`Dashboard service failed with status ${response.status}`);
+		}
+	} catch (error) {
+		console.error(`[GM] Dashboard service (via route /tournament/:tournamentID) is unreachable: ${error}`);
+		throw new Error('Dashboard service is unreachable.');
+	}
 }
