3cea775db4a825b82f84a5eda204670916a79c8f
Charlotte Sweeting <charlottesweeting@email>
2025-11-28 11:25:09 +0100

    add: sending end game to dashboad and user services but error 500 for now

--- services/game-manager/app/src/quickmatch/gameOver.ts
+++ services/game-manager/app/src/quickmatch/gameOver.ts
@@ -2,38 +2,89 @@ import { fetch, Agent } from 'undici';
 import type { game } from '../manager.interface.js';
 
 export function gameOver(payload: string) {
-    // send obj to DB
-    // const game: game = JSON.parse(payload);
-    // postGameToDashboard(game);
-    // showWinnerScreen();
+	const game: game = JSON.parse(payload);
+	postGameToDashboard(game);
+	postGameToUsers(game);
+	// showWinnerScreen();
 }
 
-const sslAgent = new Agent({
-    connect: { rejectUnauthorized: false }
-});
+interface gameDashboardReqBody {
+	gameID: string //TODO: need to update type in db
+	tournamentID?: string | undefined //TODO: does db handle undefined ?
+	localGame: boolean
+	// startTime: DATETIME //(a checker la gueule en typescript)
+	player1: number
+	player2: number
+	duration: number
+	player1Score: number
+	player2Score: number
+}
 
 async function postGameToDashboard(game: game) {
-    const url = `https://dashboard:1515/game}`;
-    let response: Response;
-
-    try {
-        response = await fetch(url, {
-            method: 'POST',
-            dispatcher: sslAgent,
-            body: JSON.stringify(game),
-        });
-    } catch (error) {
-        console.error(`[GM] Dashboard service (via route /game) is unreachable: ${error}`);
-        throw new Error('Dashboard service is unreachable.');
-    }
-
-    // TODO check response status ?
-
-    if (!response.ok) {
-        console.error(`[GM] Dashboard service failed with status: ${response.status}`);
-        throw new Error(`Dashboard service failed with status ${response.status}`);
-    }
+	const url = `http://dashboard:1515/game`;
+	const reqBody: gameDashboardReqBody = {
+		gameID: game.gameID,
+		tournamentID: game.tournamentID,
+		localGame: game.remote ? false : true,
+		player1: game.users![0]!.userID!,
+		player2: game.users![1]!.userID!,
+		duration: game.duration,
+		player1Score: game.users![0]!.userID! === game.winnerID ? game.score[0] : game.score[1],
+		player2Score: game.users![1]!.userID! === game.winnerID ? game.score[0] : game.score[1],
+	}
+	try {
+		const response: Response = await fetch(url, {
+			method: 'POST',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify(reqBody),
+		});
+		// TODO check response status ?
+		if (!response.ok) {
+			console.error(`[GM] Dashboard service failed with status: ${response.status}`);
+			throw new Error(`Dashboard service failed with status ${response.status}`);
+		}
+	} catch (error) {
+		console.error(`[GM] Dashboard service (via route /game) is unreachable: ${error}`);
+		throw new Error('Dashboard service is unreachable.');
+	}
 }
+
+interface usersReqBody {
+	player1: number;
+	player2: number;
+	player1Score: number;
+	player2Score: number;
+	longuestPass: number;
+	duration: number;
+}
+
+async function postGameToUsers(game: game) {
+	const url = `http://users:2626/stats`;
+	const reqBody: usersReqBody = {
+		player1: game.users![0]!.userID!,
+		player2: game.users![1]!.userID!,
+		player1Score: game.users![0]!.userID! === game.winnerID ? game.score[0] : game.score[1],
+		player2Score: game.users![1]!.userID! === game.winnerID ? game.score[0] : game.score[1],
+		longuestPass: game.longuestPass,
+		duration: game.duration,
+	}
+	try {
+		const response: Response = await fetch(url, {
+			method: 'PATCH',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify(reqBody),
+		});
+		// TODO check response status ?
+		if (!response.ok) {
+			console.error(`[GM] Dashboard service failed with status: ${response.status}`);
+			throw new Error(`Dashboard service failed with status ${response.status}`);
+		}
+	} catch (error) {
+		console.error(`[GM] Dashboard service (via route /game) is unreachable: ${error}`);
+		throw new Error('Dashboard service is unreachable.');
+	}
+}
+
 
 /* SAM */
 // interface game {
