bbd0f0f52523924cbf83d2cd588ac0499b319339
Charlotte Sweeting <charlottesweeting@email>
2025-12-03 11:54:55 +0100

    fix: getlogstate condition

--- services/frontend/app/src/client/web-elements/navigation/header.ts
+++ services/frontend/app/src/client/web-elements/navigation/header.ts
@@ -68,13 +68,18 @@ export class PageHeader extends HTMLElement {
         const log = await userStatus();
 
         if (log.auth) {
-            this.#login.remove();
-            this.append(this.#logout);
-            this.#logout.classList.add('h-m', 'w-l');
+            if (this.contains(this.#login)) this.#login.remove();
+            if (!this.contains(this.#logout)) {
+                this.append(this.#logout);
+                this.#logout.classList.add('h-m', 'w-l');
+            }
+            // await this.#notif.fetchPendingFriendRequests();
         } else {
-            this.#logout.remove();
-            this.append(this.#login);
-            this.#login.classList.add('h-m', 'w-l');
+            if (this.contains(this.#logout)) this.#logout.remove();
+            if (!this.contains(this.#login)) {
+                this.append(this.#login);
+                this.#login.classList.add('h-m', 'w-l');
+            }
         }
     }
 
