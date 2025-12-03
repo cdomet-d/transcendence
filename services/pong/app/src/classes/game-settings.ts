b7abb1bda3dbbd58bafbc047cf8ee1dec6f93236
Charlotte Sweeting <charlottesweeting@gmail.com>
2025-12-03 17:11:54 +0100

    fix: only have 3 levels in settings now

--- services/pong/app/src/classes/game-settings.ts
+++ services/pong/app/src/classes/game-settings.ts
@@ -8,14 +8,8 @@ export function getBallStartingSpeed(ballspeed: string): coordinates /*| null*/ 
     if (level <= 0)
 		return {x: 0.3, y: 0.03};
 	if (level === 1)
-		return {x: 0.36, y: 0.036};
-	if (level === 2)
-		return {x: 0.42, y: 0.042};
-	if (level === 3)
-        return {x: 0.48, y: 0.048};
-	if (level === 4)
-        return {x: 0.54, y: 0.054};
-	if (level >= 5)
+		return {x: 0.45, y: 0.045};
+	if (level >= 2)
         return {x: 0.6, y: 0.06};
 	return {x: 0.3, y: 0.03};
 }
@@ -25,15 +19,9 @@ export function getPaddleSpeed(paddlespeed: string): number {
 	if (level <= 0)
 		return 0.42;
 	if (level === 1)
-		return 0.496;
-	if (level === 2)
-		return 0.572;
-	if (level === 3)
-        return 0.648;
-	if (level === 4)
-        return 0.724;
-	if (level >= 5)
-        return 0.80;
+		return 0.57;
+	if (level >= 2)
+        return 0.72;
 	return 0.42;
 }
 
@@ -42,14 +30,8 @@ export function getPaddleHeight(paddlesize: string): number {
 	if (level <= 0)
 		return HEIGHT / 5;
 	if (level === 1)
-		return HEIGHT / 4.6;
-	if (level === 2)
-		return HEIGHT / 4.2;
-	if (level === 3)
-        return HEIGHT / 3.8;
-	if (level === 4)
-        return HEIGHT / 3.4;
-	if (level >= 5)
+		return HEIGHT / 4;
+	if (level >= 2)
 		return HEIGHT / 3;
 	return HEIGHT / 5;
 }
