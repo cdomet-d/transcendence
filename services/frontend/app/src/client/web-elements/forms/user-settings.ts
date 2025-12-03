9b2830788072835d235b05ecffd2eb0a422b5666
Jauppp <coralie.dometdemont@gmail.com>
2025-12-03 09:21:34 +0100

    chore: commented unfinished feature

--- services/frontend/app/src/client/web-elements/forms/user-settings.ts
+++ services/frontend/app/src/client/web-elements/forms/user-settings.ts
@@ -14,8 +14,8 @@ import {
     errorMessageFromException,
     errorMessageFromResponse,
 } from '../../error.js';
-import { Popup } from '../layouts/popup.js';
-import { sensitiveAccountChange } from './default-forms.js';
+// import { Popup } from '../layouts/popup.js';
+// import { sensitiveAccountChange } from './default-forms.js';
 // import imageCompression from 'browser-image-compression';
 
 const MAX_FILE = 2 * 1024 * 1024;
@@ -60,17 +60,17 @@ export class UserSettingsForm extends BaseForm {
         this.contentMap.get('upload')?.removeEventListener('input', this.#previewAvatar);
     }
 
-    override createReqBody(form: FormData): string {
-        const fObject = Object.fromEntries(form.entries());
+    // override createReqBody(form: FormData): string {
+    //     const fObject = Object.fromEntries(form.entries());
 
-        if (fObject.username || fObject.password) {
-			console.log('Username or pw change')
-            const dialog = document.createElement('dialog', { is: 'custom-popup' }) as Popup;
-            dialog.appendAndCache(createForm('pw-form', sensitiveAccountChange));
-            this.append(dialog);
-        }
-		return 'AAAAAAAAAAh';
-    }
+    //     if (fObject.username || fObject.password) {
+	// 		console.log('Username or pw change')
+    //         const dialog = document.createElement('dialog', { is: 'custom-popup' }) as Popup;
+    //         dialog.appendAndCache(createForm('pw-form', sensitiveAccountChange));
+    //         this.append(dialog);
+    //     }
+	// 	return 'AAAAAAAAAAh';
+    // }
 
     override async fetchAndRedirect(url: string, req: RequestInit): Promise<void> {
         console.log(url, req);
