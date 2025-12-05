9bd54bcb20309f2b4da94213a64f14582e494a95
Alexie Julien <alexiejulien@macbookair.home>
2025-12-05 22:40:54 +0100

    add: schemas and sanitizing in accessibility service

--- services/accessibility/app/src/routes.ts
+++ services/accessibility/app/src/routes.ts
@@ -1,15 +1,17 @@
 import type { FastifyInstance } from 'fastify';
+import { getDictionarySchema } from './schemas.js';
+import { cleanInput } from './sanitizer.js';
 
 export async function languageRoutes(serv: FastifyInstance) {
 
-	serv.get('/dictionary/:langCode', async (request, reply) => {
+	serv.get('/dictionary/:langCode', { schema: getDictionarySchema }, async (request, reply) => {
 		const { langCode } = request.params as { langCode: string };
+		const lang = cleanInput(langCode);
 
 		const query = `SELECT pack_json FROM language_packs WHERE language_code = ?`;
 		
 		try {
-			const result = await serv.dbLanguage.get<{ pack_json: string }>(query, [langCode]);
-			console.log(JSON.stringify(result));
+			const result = await serv.dbLanguage.get<{ pack_json: string }>(query, [lang]);
 
 			if (!result)
 				return reply.code(404).send({ message: 'Language pack not found' });
