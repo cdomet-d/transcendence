c8606deba7ee3d6b3af1af092cd54089fa695af2
Charlotte Sweeting <charlottesweeting@email>
2025-12-06 21:56:19 +0100

    fix: replaced -1 with F in score

--- services/frontend/app/src/client/api-responses/matches.ts
+++ services/frontend/app/src/client/api-responses/matches.ts
@@ -15,6 +15,8 @@ export function MatchOutcomeArrayFromAPIRes(responseObject: any): MatchOutcome[]
 	let matchCollection: MatchOutcome[] = [];
 	responseObject.forEach((match: any) => {
 		const m = matchOutcomeFromAPIRes(match);
+		if (m.score.includes("-1"))
+			m.score = m.score.replace("-1", "F");
 		matchCollection.push(m);
 	});
 	return matchCollection;
