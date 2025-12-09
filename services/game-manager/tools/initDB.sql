dec6ed59ad3d17b9bb7cf5fb2f2d70f33f60dbc3
Charlotte Sweeting <charlottesweeting@email>
2025-12-09 10:55:48 +0100

    fix: rm extra coma

--- services/game-manager/tools/initDB.sql
+++ services/game-manager/tools/initDB.sql
@@ -1,7 +1,7 @@
-CREATE TABLE IF NOT EXISTS gameInviteNotifs (
+CREATE TABLE gameInviteNotifs (
     type TEXT,
     senderUsername TEXT,
     receiverID TEXT,
     lobbyID TEXT,
-    gameType TEXT,
-);
+    gameType TEXT
+)
