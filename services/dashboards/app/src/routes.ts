abd716840fbcb6fca5c5dc6c1c5eb817f7985bf3
Eiixela <alexielaurine@live.fr>
2025-12-14 10:06:39 +0100

    fix: duration

--- services/dashboards/app/src/routes.ts
+++ services/dashboards/app/src/routes.ts
@@ -43,6 +43,11 @@ export async function dashboardRoutes(serv: FastifyInstance) {
 				body.duration = Math.trunc(Number(body.duration));
 			}
 
+
+			if (!body.startTime) {
+				body.duration = "";
+			}
+
 			const validKeys = [
 				'gameID',
 				'tournamentID',
@@ -123,7 +128,7 @@ export async function dashboardRoutes(serv: FastifyInstance) {
 			const safeUserID = cleanInput(userID);
 			if (!safeUserID)
 				return (reply.code(400).send({ message: '[DASHBOARD] userID query parameter is required.' }));
-			
+
 			const games = await getGameHistory(serv.dbStats, safeUserID);
 			return (reply.code(200).send(games));
 
